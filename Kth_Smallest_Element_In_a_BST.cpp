f3b654486e3bc3afee6311cf75461b993ac25dae
Singhal_Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2022-04-18 20:53:03 +0530

    Create Kth_Smallest_Element_In_a_BST.cpp

---
+++ Kth_Smallest_Element_In_a_BST.cpp
@@ -0,0 +1,16 @@
+Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+
+ class Solution {
+    int res,count;
+    public int kthSmallest(TreeNode root, int k) {
+        count=k;
+        inorder(root);
+        return res;
+    }
+    void inorder(TreeNode root){
+        if(root ==null) return ;
+        inorder(root.left);
+        if(--count==0) res=root.val;
+        inorder(root.right);
+    }
+}
