657f8ec031087fafe553c4c76bb41f25c8520ed5
Singhal_Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2022-04-18 20:54:40 +0530

    Update Kth_Smallest_Element_In_a_BST.cpp

--- Kth_Smallest_Element_In_a_BST.cpp
+++ Kth_Smallest_Element_In_a_BST.cpp
@@ -1,16 +1,16 @@
 Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
 
  class Solution {
-    int res,count;
-    public int kthSmallest(TreeNode root, int k) {
-        count=k;
-        inorder(root);
+public:
+    int res;
+    int kthSmallest(TreeNode* root, int k) {
+        inorder(root,k);
         return res;
     }
-    void inorder(TreeNode root){
-        if(root ==null) return ;
-        inorder(root.left);
-        if(--count==0) res=root.val;
-        inorder(root.right);
+    void inorder(TreeNode*root,int &k){
+        if(!root)return ;
+        inorder(root->left,k);
+        if(--k==0)res=root->val;
+        inorder(root->right,k);
     }
-}
+};
