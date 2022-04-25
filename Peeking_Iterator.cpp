5bb822940f64b9321a0c9887366338def0057b26
Singhal_Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2022-04-25 22:48:29 +0530

    Create Peeking_Iterator.cpp

---
+++ Peeking_Iterator.cpp
@@ -0,0 +1,63 @@
+Design an iterator that supports the peek operation on an existing iterator in addition to the hasNext and the next operations.
+
+Implement the PeekingIterator class:
+
+PeekingIterator(Iterator<int> nums) Initializes the object with the given integer iterator iterator.
+int next() Returns the next element in the array and moves the pointer to the next element.
+boolean hasNext() Returns true if there are still elements in the array.
+int peek() Returns the next element in the array without moving the pointer.
+Note: Each language may have a different implementation of the constructor and Iterator, but they all support the int next() and boolean hasNext() functions.
+  
+  /*
+ * Below is the interface for Iterator, which is already defined for you.
+ * **DO NOT** modify the interface for Iterator.
+ *
+ *  class Iterator {
+ *		struct Data;
+ * 		Data* data;
+ *  public:
+ *		Iterator(const vector<int>& nums);
+ * 		Iterator(const Iterator& iter);
+ *
+ * 		// Returns the next element in the iteration.
+ *		int next();
+ *
+ *		// Returns true if the iteration has more elements.
+ *		bool hasNext() const;
+ *	};
+ */
+
+class PeekingIterator : public Iterator {
+        int next_val;
+    bool iter_hasnext;
+public:
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+	    // Initialize any member here.
+	    // **DO NOT** save a copy of nums and manipulate it directly.
+	    // You should only use the Iterator interface methods.
+	    iter_hasnext=Iterator::hasNext();
+        if(iter_hasnext){
+            next_val=Iterator::next();
+        }
+	}
+	
+    // Returns the next element in the iteration without advancing the iterator.
+	int peek() {
+        return next_val;
+	}
+	
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	int next() {
+	    int curr=next_val;
+        iter_hasnext=Iterator::hasNext();
+        if(iter_hasnext){
+            next_val=Iterator::next();
+        }
+        return curr;
+	}
+	
+	bool hasNext() const {
+	    return iter_hasnext;
+	}
+};
