832b843d854934f93c90c440971dd2d0c7b4882c
Singhal-Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2022-12-29 12:54:24 +0530

    Time: 8 ms (59.25%), Space: 16.4 MB (50.04%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -11,24 +11,34 @@
  */
 class Solution {
 public:
+    
+    bool recurse(TreeNode*p,TreeNode*q){
+        if(!p && !q)return true;
+        if(!p || !q)return false;
+        if(p->val!=q->val)return false;
+        return recurse(p->left,q->right)&& recurse(p->right,q->left);
+        
+    }
+    
     bool isSymmetric(TreeNode* root) {
         
-        queue<TreeNode*>q1,q2;
-        q1.push(root->left),q2.push(root->right);
-        while(!q1.empty() && !q2.empty()){
-            auto left=q1.front();
-            q1.pop();
-            auto right=q2.front();
-            q2.pop();
-            if(left==NULL && right==NULL)continue;
-            if(left==NULL || right==NULL)return false;
-            if(left->val!=right->val)return false;
-            q1.push(left->left);
-            q1.push(left->right);
-            q2.push(right->right);
-            q2.push(right->left);
-        }
-        return true;
+        // queue<TreeNode*>q1,q2;
+        // q1.push(root->left),q2.push(root->right);
+        // while(!q1.empty() && !q2.empty()){
+        //     auto left=q1.front();
+        //     q1.pop();
+        //     auto right=q2.front();
+        //     q2.pop();
+        //     if(left==NULL && right==NULL)continue;
+        //     if(left==NULL || right==NULL)return false;
+        //     if(left->val!=right->val)return false;
+        //     q1.push(left->left);
+        //     q1.push(left->right);
+        //     q2.push(right->right);
+        //     q2.push(right->left);
+        // }
+        // return true;
+        return recurse(root->left,root->right);
         
     }
 };
