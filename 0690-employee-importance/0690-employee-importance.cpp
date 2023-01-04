c7a2ee4d974a55c5ad6c18deaef66d49469115ca
Singhal-Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2023-01-04 15:15:04 +0530

    Time: 733 ms (5.00%), Space: 100.5 MB (6.09%) - LeetHub

---
+++ 0690-employee-importance/0690-employee-importance.cpp
@@ -0,0 +1,30 @@
+/*
+// Definition for Employee.
+class Employee {
+public:
+    int id;
+    int importance;
+    vector<int> subordinates;
+};
+*/
+
+class Solution {
+public:
+    int sum=0;
+    void dfs(unordered_map<int,Employee*>map,int id){
+        sum+=map[id]->importance;
+        for(auto e:map[id]->subordinates){
+            // sum+=e->importance;
+            dfs(map,e);
+        }
+    }
+    
+    int getImportance(vector<Employee*> employees, int id) {
+        unordered_map<int,Employee*>map;
+        for(auto e:employees)map[e->id]=e;
+        dfs(map,id);
+        return sum;
+        
+        
+    }
+};
