cf71abd44c595e3862f3ebb5c57772d5e2b79c5e
Singhal-Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2023-01-04 15:32:30 +0530

    Time: 64 ms (43.44%), Space: 14 MB (82.66%) - LeetHub

--- 0690-employee-importance/0690-employee-importance.cpp
+++ 0690-employee-importance/0690-employee-importance.cpp
@@ -11,18 +11,35 @@ public:
 class Solution {
 public:
     int sum=0;
-    void dfs(unordered_map<int,Employee*>&map,int id){
-        sum+=map[id]->importance;
-        for(auto e:map[id]->subordinates){
-            // sum+=e->importance;
-            dfs(map,e);
-        }
-    }
+    // void dfs(unordered_map<int,Employee*>&map,int id){
+    //     sum+=map[id]->importance;
+    //     for(auto e:map[id]->subordinates){
+    //         // sum+=e->importance;
+    //         dfs(map,e);
+    //     }
+    // }
     
     int getImportance(vector<Employee*> employees, int id) {
         unordered_map<int,Employee*>map;
         for(auto e:employees)map[e->id]=e;
-        dfs(map,id);
+        
+        queue<int>q;
+        q.push(id);
+        while(!q.empty()){
+            
+            int n=q.size();
+            for(int i=0;i<n;i++){
+            int top=q.front();
+            q.pop();
+                sum+=map[top]->importance;
+                for(int j:map[top]->subordinates){
+                    q.push(j);
+                }
+                
+            }
+        }
+        
+        // dfs(map,id);
         return sum;
         
         
