e3a455800ae2d585ae184c8ac74e25503a3fbca3
Singhal-Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2023-01-04 23:44:12 +0530

    Time: 201 ms (99.28%), Space: 105.9 MB (8.47%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -6,21 +6,28 @@ public:
         for(int i:tasks){
             map[i]++;
         }
-        priority_queue<pair<int,long long>>pq;
+        // priority_queue<pair<int,long long>>pq;
+        // for(auto m:map){
+        //     pq.push({m.second,m.first});
+        // }
+//         while(!pq.empty()){
+//             // pair<int,long long>f=pq.top();pq.pop();
+//             if(f.first==1)return -1;
+            
+//             if(f.first==4)f.first-=2;
+//             else f.first-=3;
+            
+//             if(f.first>0)pq.push({f.first,f.second});
+//             min++;
+            
+//             }
+        
         for(auto m:map){
-            pq.push({m.second,m.first});
+            if(m.second==1)return -1;
+            else if(m.second%3==0) min+=m.second/3;
+            else min+= m.second/3+1;
         }
-        while(!pq.empty()){
-            pair<int,long long>f=pq.top();pq.pop();
-            if(f.first==1)return -1;
-            
-            if(f.first==4)f.first-=2;
-            else f.first-=3;
-            
-            if(f.first>0)pq.push({f.first,f.second});
-            min++;
-            
-            }
+        
         return min;
     }
 };
