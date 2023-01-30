5b0320cecb1cc59a1a9abbfb8a917e29a10f590a
Singhal-Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2023-01-30 20:59:49 +0530

    Time: 237 ms (64.13%), Space: 47.3 MB (64.76%) - LeetHub

--- 1705-maximum-number-of-eaten-apples/1705-maximum-number-of-eaten-apples.cpp
+++ 1705-maximum-number-of-eaten-apples/1705-maximum-number-of-eaten-apples.cpp
@@ -2,37 +2,23 @@ class Solution {
 public:
     int eatenApples(vector<int>& apples, vector<int>& days) {
         
-        int globalDays=0,n=apples.size(),cnt=0;
+        int i=0,n=apples.size(),cnt=0;
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-       for(int i=0;i<n;i++){
-           globalDays++;
-           pq.push({i+days[i],apples[i]});
-           
-           while(!pq.empty() && pq.top().first<globalDays){
-               pq.pop();
-           }
-           if(!pq.empty()){
-               auto [f,s]=pq.top();pq.pop();
-               cnt+=1;
-               if(--s>0)
-               pq.push({f,s});
-           }
-           
-       }
-            while(!pq.empty()){
-                
-            globalDays++;
-                cout<<globalDays<<" "<<pq.top().second;
-            while(!pq.empty() && pq.top().first<globalDays){
-                pq.pop();
+        
+        while(i<n || !pq.empty()){
+            if(i<n && apples[i]!=0)
+            pq.push({i+days[i],apples[i]});
+        
+            while(!pq.empty() && pq.top().first<i+1)pq.pop();
+            
+            if(!pq.empty()){
+                auto [first,sec]=pq.top();pq.pop();
+                cnt++;
+                if(--sec>0)pq.push({first,sec});
+                }
+            i++;
             }
-              if(!pq.empty()){
-                auto [f,s]=pq.top();pq.pop();
-                    cnt+=1;
-                  if(--s>0)
-                     pq.push({f,s});
-           }   
-        }
+        
         return cnt;
         
     }
