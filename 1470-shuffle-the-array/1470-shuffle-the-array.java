9722060367904a82356167c547566a2849dbb020
Singhal-Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2023-02-06 10:54:49 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (51.87%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,13 +1,22 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
         
+        // for(int i=n;i<nums.length;i++){
+        //     nums[i]=nums[i]*1024 + nums[i-n];
+        // }
+        // int idx=0;
+        // for(int i=n;i<nums.length;i++,idx+=2){
+        //     nums[idx]=nums[i]%1024;
+        //     nums[idx+1]=nums[i]/1024;
+        // }
+        // return nums;
         for(int i=n;i<nums.length;i++){
-            nums[i]=nums[i]*1024 + nums[i-n];
+            nums[i]=(nums[i]<<10)|nums[i-n];
         }
         int idx=0;
         for(int i=n;i<nums.length;i++,idx+=2){
-            nums[idx]=nums[i]%1024;
-            nums[idx+1]=nums[i]/1024;
+            nums[idx]=nums[i]&1023;
+            nums[idx+1]=nums[i]>>10;
         }
         return nums;
     }
