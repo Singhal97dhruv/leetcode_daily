399bb84c5f4a9a36528efad6a7c9a923ede84d1a
Singhal-Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2023-02-16 18:58:28 +0530

    Time: 9 ms (36.58%), Space: 10.2 MB (82.75%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
-    void unique(string curr,int n,int idx,unordered_set<string>&set,string &ans){
+//     void unique(string curr,int n,int idx,unordered_set<string>&set,string &ans){
         
-        if(idx==n){
-            if(set.find(curr)==set.end())ans=curr;
-            return;
-        }
-        if(ans=="")
-        unique(curr+"0",n,idx+1,set,ans);
-        if(ans=="")
-        unique(curr+"1",n,idx+1,set,ans);
+//         if(idx==n){
+//             if(set.find(curr)==set.end())ans=curr;
+//             return;
+//         }
+//         if(ans=="")
+//         unique(curr+"0",n,idx+1,set,ans);
+//         if(ans=="")
+//         unique(curr+"1",n,idx+1,set,ans);
 
         
-    }
+//     }
     
     string findDifferentBinaryString(vector<string>& nums) {
-        unordered_set<string>set;
-        string ans="";
-        for(auto st:nums){
-            set.insert(st);
+        // unordered_set<string>set;
+        // string ans="";
+        // for(auto st:nums){
+        //     set.insert(st);
+        // }
+        // unique("",nums.size(),0,set,ans);
+        // return ans;
+        
+        string ans=nums[0];
+        for(int i=0;i<nums.size();i++){
+            ans[i]=nums[i][i]=='0'?'1':'0';
         }
-        unique("",nums.size(),0,set,ans);
         return ans;
+        
     }
 };
