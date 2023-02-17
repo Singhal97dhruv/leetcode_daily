b761077080fc758ff0780a9081eccc28644ee7a5
Singhal-Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2023-02-18 03:25:18 +0530

    Time: 4 ms (53.33%), Space: 7.2 MB (97.97%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -2,11 +2,20 @@ class Solution {
 public:
     vector<string> fizzBuzz(int n) {
         vector<string>vect(n,"");
-        for(int i=1;i<=n;i++){
-            if(i%3==0 && i%5==0)vect[i-1]="FizzBuzz";
-            else if(i%3==0)vect[i-1]="Fizz";
-            else if(i%5==0)vect[i-1]="Buzz";
-            else vect[i-1]=to_string(i);
+        // for(int i=1;i<=n;i++){
+        //     if(i%3==0 && i%5==0)vect[i-1]="FizzBuzz";
+        //     else if(i%3==0)vect[i-1]="Fizz";
+        //     else if(i%5==0)vect[i-1]="Buzz";
+        //     else vect[i-1]=to_string(i);
+        // }
+        // return vect;
+        for(int i=0;i<n;i++){
+            vect[i]=to_string(i+1);
+        }
+        for(int i=3;i<=n;i=i+3)vect[i-1]="Fizz";
+        for(int i=5;i<=n;i=i+5)vect[i-1]="Buzz";
+        for(int i=15;i<=n;i=i+15){
+            vect[i-1]="FizzBuzz";
         }
         return vect;
     }
