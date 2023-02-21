40963f3d104f85c08ed7c109d6772d437e22ca87
Singhal-Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2023-02-21 22:59:27 +0530

    Time: 4 ms (85.71%), Space: 54.5 MB (22.27%) - LeetHub

--- 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.java
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.java
@@ -1,12 +1,11 @@
 class Solution {
-public:
-    int countNodes(vector<int>&left,vector<int>&right,int root){
+    public int countNodes(int[] left,int[] right, int root){
         if(root==-1)return 0;
         return 1+countNodes(left,right,left[root])+countNodes(left,right,right[root]);
     }
-    bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
-        
-        vector<int>vis(n,0);int root=-1;
+    public boolean validateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild) {
+        int[] vis=new int[n];
+        int root=-1;
         for(int i=0;i<n;i++){
             if(leftChild[i]!=-1 && vis[leftChild[i]]++==1)return false;
             if(rightChild[i]!=-1 && vis[rightChild[i]]++==1)return false;
@@ -19,4 +18,4 @@ public:
         if(root==-1)return false;
         return countNodes(leftChild,rightChild,root)==n;
     }
-};
+}
