6316d7922ac7008697e1f47d6ab83a474d776bfd
Singhal-Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2023-02-24 23:20:28 +0530

    Time: 161 ms (17.44%), Space: 58.6 MB (23.83%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
@@ -1,13 +1,13 @@
 class Solution {
-public:
-    int maximumCandies(vector<int>& candies, long long k) {
-        int left=0,right=*max_element(candies.begin(),candies.end());
+    public int maximumCandies(int[] candies, long k) {
+        int left=0,right=10000000;
         while(left<right){
-            long cnt=0, mid=(left+right+1)/2;
-            for(long long i:candies)cnt+=i/mid;
+            int mid=(left+right+1)/2;
+            long cnt=0;
+            for(long i:candies)cnt+=i/mid;
             if(cnt>=k)left=mid;
             else right=mid-1;
         }
         return left;
     }
-};
+}
