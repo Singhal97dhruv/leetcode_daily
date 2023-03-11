5521d5674a4f041c5b7061c7732a1315271e116b
Singhal-Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2023-03-11 21:04:30 +0530

    Time: 45 ms (54.59%), Space: 22.2 MB (52.45%) - LeetHub

---
+++ 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class FindElements {
+public:
+    unordered_map<int,int>map;
+    void dfs(TreeNode*t){
+        // if(t==NULL)return;
+        // if(t->val==-1)t->val=0;
+        if(t->left){
+            t->left->val=2*t->val+1;
+            map[t->left->val]++;
+            dfs(t->left);
+        }
+        if(t->right){
+            t->right->val=2*t->val+2;
+            map[t->right->val]++;
+            dfs(t->right);
+        }
+        
+    }
+    
+    FindElements(TreeNode* root) {
+        root->val=0;
+        map[0]++;
+        dfs(root);
+    }
+    
+    bool find(int target) {
+        if(map[target]>0)return true;
+        return false;
+    }
+};
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * FindElements* obj = new FindElements(root);
+ * bool param_1 = obj->find(target);
+ */
