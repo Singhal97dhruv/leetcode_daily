7b8780927db5097a1c8c9dd797e420b694757960
Singhal-Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2023-03-13 22:43:47 +0530

    Time: 289 ms (66.92%), Space: 172.8 MB (43.85%) - LeetHub

--- 1738-find-kth-largest-xor-coordinate-value/1738-find-kth-largest-xor-coordinate-value.java
+++ 1738-find-kth-largest-xor-coordinate-value/1738-find-kth-largest-xor-coordinate-value.java
@@ -1,7 +1,7 @@
 class Solution {
     public int kthLargestValue(int[][] matrix, int k) {
         int m=matrix.length,n=matrix[0].length;
-        PriorityQueue<Integer>pq=new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer>pq=new PriorityQueue<>();
         pq.add(matrix[0][0]);
         for(int i=1;i<m;i++){
             matrix[i][0]^=matrix[i-1][0];
@@ -12,15 +12,17 @@ class Solution {
             matrix[0][j]^=matrix[0][j-1];
             pq.add(matrix[0][j]);
         }
+        while(pq.size()>k)pq.poll();
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
                 matrix[i][j]=matrix[i][j]^matrix[i-1][j]^matrix[i][j-1]^matrix[i-1][j-1];
                 pq.add(matrix[i][j]);
+                if(pq.size()>k)pq.poll();
             }
         }
-        while(--k>0){
-            pq.poll();
-        }       
+        // while(--k>0){
+        //     pq.poll();
+        // }       
         return pq.peek().intValue();
     }
 }
