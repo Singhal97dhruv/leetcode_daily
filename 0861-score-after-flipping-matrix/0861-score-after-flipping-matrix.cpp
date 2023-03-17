fbce7349c70cd4e2a9f6f745c4737d828af1b1fb
Singhal-Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2023-03-18 00:22:01 +0530

    Time: 3 ms (71.37%), Space: 8.2 MB (66.59%) - LeetHub

--- 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.cpp
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.cpp
@@ -1,41 +1,58 @@
 class Solution {
 public:
-    void flipRow(vector<vector<int>>&g,int i){
-        g[i][0]=1;
-        for(int j=1;j<g[0].size();j++){
-            g[i][j]=!g[i][j];
-        }
-    }
-    void flipCol(vector<vector<int>>&g,int j){
-        for(int i=0;i<g.size();i++){
-            g[i][j]=!g[i][j];
-        }
-    }
+//     void flipRow(vector<vector<int>>&g,int i){
+//         g[i][0]=1;
+//         for(int j=1;j<g[0].size();j++){
+//             g[i][j]=!g[i][j];
+//         }
+//     }
+//     void flipCol(vector<vector<int>>&g,int j){
+//         for(int i=0;i<g.size();i++){
+//             g[i][j]=!g[i][j];
+//         }
+//     }
     
     int matrixScore(vector<vector<int>>& grid) {
         int m=grid.size(),n=grid[0].size();
-        vector<int>cols(n,0);
-        for(int i=0;i<m;i++){
-            if(grid[i][0]==0){
-                flipRow(grid,i);
-            }
-            for(int j=1;j<n;j++){
-                if(grid[i][j])cols[j]++;
-            }
-        }
+//         vector<int>cols(n,0);
+//         for(int i=0;i<m;i++){
+//             if(grid[i][0]==0){
+//                 flipRow(grid,i);
+//             }
+//             for(int j=1;j<n;j++){
+//                 if(grid[i][j])cols[j]++;
+//             }
+//         }
         
-        for(int j=1;j<n;j++){
-            if(cols[j]<=m/2)flipCol(grid,j);
-        }
+//         for(int j=1;j<n;j++){
+//             if(cols[j]<=m/2)flipCol(grid,j);
+//         }
+        
+//         int res=0;
+//         for(int i=0;i<m;i++){
+//             int sum=0;
+//             for(int j=n-1;j>=0;j--){
+//                 sum+=grid[i][j]==1?pow(2,n-1-j):0;
+//             }
+//             res+=sum;
+//         }
+//         return res;
         
         int res=0;
-        for(int i=0;i<m;i++){
-            int sum=0;
-            for(int j=n-1;j>=0;j--){
-                sum+=grid[i][j]==1?pow(2,n-1-j):0;
+        res+=(1<<(n-1))*m;
+        
+        for(int j=1;j<n;j++){
+            int sm=0;
+            for(int i=0;i<m;i++){
+                if(grid[i][0]==grid[i][j])sm++;
             }
-            res+=sum;
+            res+=(1<<n-1-j)*max(sm,m-sm);
         }
         return res;
+        
+        
+        
+        
+        
     }
 };
