05a02f74190002fe2e64af5a4d32792bcff722a5
Singhal-Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2023-03-20 02:00:05 +0530

    Time: 199 ms (26.97%), Space: 36.9 MB (46.06%) - LeetHub

--- 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     vector<pair<int,int>>offset={{0,1},{1,0},{0,-1},{-1,0}};
     int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
-        set<pair<int,int>>set;
+        // set<pair<int,int>>set;
+        unordered_set<string>set;
         for(vector<int>v:obstacles){
-            set.insert({v[0],v[1]});
+            // set.insert({v[0],v[1]});
+            set.insert(to_string(v[0])+"#"+to_string(v[1]));
         }
         int k=0,x=0,y=0,res=0;
         for(int i:commands){
             if(i==-1)k=(k+1)%4;
             else if(i==-2)k=(k-1+4)%4;
             for(int j=1;j<=i;j++){
-                if(set.find({x+offset[k].first,y+offset[k].second})!=set.end())break;
+                if(set.find(to_string(x+offset[k].first) +"#"+to_string(y+offset[k].second))!=set.end())break;
                 x=x+offset[k].first;y=y+offset[k].second;
                 
             }
