ee226f6c84d0e6603b49acadae7001d30a527e2f
Singhal-Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2023-03-24 22:59:19 +0530

    Time: 18 ms (48.76%), Space: 11.1 MB (20.46%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,28 +1,15 @@
 class Solution {
 public:
-    struct cmp{
-        bool operator()(vector<int>&a,vector<int>&b){
-            if(a[1]<b[1] || (a[1]==b[1] && a[2]<b[2]))return true;
-            return false;
-        }
-    };
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        sort(trips.begin(),trips.end(),cmp());
+     
         int cap=0;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        map<int,int>map;
         for(vector<int>v:trips){
-            if(pq.empty()){
-                cap+=v[0];
-                pq.push({v[2],v[0]});
-            }
-            else{
-                while(!pq.empty() && pq.top().first<=v[1]){
-                    cap-=pq.top().second;
-                    pq.pop();
-                }
-                cap+=v[0];
-                pq.push({v[2],v[0]});
-            }
+            map[v[1]]+=v[0];
+            map[v[2]]-=v[0];
+        }
+        for(auto i:map){
+            cap+=i.second;
             if(cap>capacity)return false;
         }
         return true;
