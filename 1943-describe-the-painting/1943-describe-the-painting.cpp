cefb8c66423c461083b5bb68acb9069efa2c035a
Singhal-Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2023-03-27 20:24:01 +0530

    Time: 556 ms (30.89%), Space: 109.1 MB (43.09%) - LeetHub

--- 1943-describe-the-painting/1943-describe-the-painting.cpp
+++ 1943-describe-the-painting/1943-describe-the-painting.cpp
@@ -2,25 +2,40 @@ class Solution {
 public:
     vector<vector<long long>> splitPainting(vector<vector<int>>& segments) {
         
-       long long arr[100002]={0};
-        bool seen[100002]={};
-        for(auto i:segments){
-            arr[i[0]]+=i[2];
-            arr[i[1]]-=i[2];
-            seen[i[0]]=seen[i[1]]=true;
-        }
+//        long long arr[100002]={0};
+//         bool seen[100002]={};
+//         for(auto i:segments){
+//             arr[i[0]]+=i[2];
+//             arr[i[1]]-=i[2];
+//             seen[i[0]]=seen[i[1]]=true;
+//         }
         
-        long long sum=0,last_idx=0;
+//         long long sum=0,last_idx=0;
+//         vector<vector<long long>>res;
+//         for(int i=0;i<100002;i++){
+//             if(sum && seen[i]){
+//                 res.push_back({last_idx,i,sum});
+//             }
+//             sum+=arr[i];
+//             last_idx=seen[i]?i:last_idx;
+//         }
+//         return res;
+        
+        map<int,long long>map;
+        for(auto i:segments){
+            map[i[0]]+=i[2];
+            map[i[1]]-=i[2];
+        }
         vector<vector<long long>>res;
-        for(int i=0;i<100002;i++){
-            if(sum && seen[i]){
-                res.push_back({last_idx,i,sum});
+        int i=0;
+        for(auto j:map){
+            if(map[i]>0){
+                res.push_back({i,j.first,map[i]});
             }
-            sum+=arr[i];
-            last_idx=seen[i]?i:last_idx;
+            map[j.first]+=map[i];
+            i=j.first;
         }
         return res;
-        
         
     }
 };
