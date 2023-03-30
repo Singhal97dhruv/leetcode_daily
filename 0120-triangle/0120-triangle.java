d1477a285a26cbee6a6b70472e09ede079eb51e7
Singhal-Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2023-03-31 00:19:34 +0530

    Time: 5 ms (43.19%), Space: 42.3 MB (40.40%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,24 +1,32 @@
 class Solution {
     
-    public int memorization(int i,int j,int n,int [][]mem,List<List<Integer>>triangle){
-        if(i==n)return 0;
+//     public int memorization(int i,int j,int n,int [][]mem,List<List<Integer>>triangle){
+//         if(i==n)return 0;
 
-        if(mem[i][j]!=-1)return mem[i][j];
+//         if(mem[i][j]!=-1)return mem[i][j];
         
-        int left=triangle.get(i).get(j)+memorization(i+1,j,n,mem,triangle);
-        int right=triangle.get(i).get(j)+memorization(i+1,j+1,n,mem,triangle);
-        return mem[i][j]=Math.min(left,right);
+//         int left=triangle.get(i).get(j)+memorization(i+1,j,n,mem,triangle);
+//         int right=triangle.get(i).get(j)+memorization(i+1,j+1,n,mem,triangle);
+//         return mem[i][j]=Math.min(left,right);
         
-    }
+//     }
     
     public int minimumTotal(List<List<Integer>> triangle) {
         int n=triangle.size();
         int [][]dp=new int[n][n];
-        for(int[]d:dp)
-            Arrays.fill(d,-1);
+        // for(int[]d:dp)
+            // Arrays.fill(d,-1);
         
-        return memorization(0,0,n,dp,triangle);
+        // return memorization(0,0,n,dp,triangle);
+
+        for(int j=0;j<n;j++)dp[n-1][j]=triangle.get(n-1).get(j);
         
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                dp[i][j]=Math.min(dp[i+1][j]+triangle.get(i).get(j),dp[i+1][j+1]+triangle.get(i).get(j));
+            }
+        }
+        return dp[0][0];
         
     }
 }
