dc39024c6cd64b58c99b69fe9614e7abe1a28fcc
Singhal-Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2023-03-30 20:50:57 +0530

    Time: 37 ms (63.15%), Space: 42.5 MB (24.90%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numSquares(int n) {
+        
+        int[]cntPerfectSquares=new int[n+1];
+        Arrays.fill(cntPerfectSquares,Integer.MAX_VALUE);
+       cntPerfectSquares[0]=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j*j<=i;j++){
+                cntPerfectSquares[i]=Math.min(cntPerfectSquares[i],cntPerfectSquares[i-j*j]+1);
+            }
+        }
+        return cntPerfectSquares[n];
+    }
+}
