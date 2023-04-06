816f775d6f5839d44da844a59b68bdf7f917c23f
Singhal-Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2023-04-07 02:05:13 +0530

    Time: 15 ms (99.02%), Space: 9.5 MB (98.55%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -6,18 +6,25 @@ public:
         for(int i:nums)sum+=i;
         if((sum&1)==1)return false;
         sum/=2;
-        vector<vector<bool>>dp(n+1,vector<bool>(sum+1));
+//         vector<vector<bool>>dp(n+1,vector<bool>(sum+1));
         
-        dp[0][0]=true;
+//         dp[0][0]=true;
         
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=sum;j++){
-                dp[i][j]=dp[i-1][j];
-                if(j>=nums[i-1]){
-                    dp[i][j]=dp[i][j]||dp[i-1][j-nums[i-1]];
-                }
-            }
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=sum;j++){
+//                 dp[i][j]=dp[i-1][j];
+//                 if(j>=nums[i-1]){
+//                     dp[i][j]=dp[i][j]||dp[i-1][j-nums[i-1]];
+//                 }
+//             }
+//         }
+//         return dp[n][sum];
+        
+        bitset<10001>dp(1);
+        for(int i:nums){
+            dp|=dp<<i;
         }
-        return dp[n][sum];
+        return dp[sum];
+        
     }
 };
