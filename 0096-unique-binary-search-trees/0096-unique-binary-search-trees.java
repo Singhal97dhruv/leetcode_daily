ec4f33d434219c37788afdca7cf5b6be7ae642a9
Singhal-Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2023-04-25 01:23:21 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (77.65%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,11 +1,16 @@
 class Solution {
-    
-    public int numTrees(int n) {
-        if(n<=1)return 1;
-        int ans=0;
-        for(int i=1;i<=n;i++){
-            ans+= numTrees(i-1)*numTrees(n-i);
+    int []dp=new int[20];
+    public int memo(int n){
+        dp[0]=dp[1]=1;
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                dp[i]+=dp[j-1]*dp[i-j];
+            }
         }
-        return ans;
+      
+        return dp[n];
+    }
+    public int numTrees(int n) {
+        return memo(n);
     }
 }
