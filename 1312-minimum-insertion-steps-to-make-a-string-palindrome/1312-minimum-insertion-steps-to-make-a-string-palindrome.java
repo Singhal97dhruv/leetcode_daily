0d518fe7c5052557e1dc746c2114874bbccf676a
Singhal-Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2023-04-26 23:08:19 +0530

    Time: 28 ms (60.98%), Space: 44.1 MB (23.98%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,13 +1,23 @@
 class Solution {
-    int dp[][];
-    public int memo(String s,int i,int j){
-        if(i>=j)return 0;
+//     int dp[][];
+//     public int memo(String s,int i,int j){
+//         if(i>=j)return 0;
     
+//         if(dp[i][j]!=-1)return dp[i][j];
+        
+//         if(s.charAt(i)==s.charAt(j))return dp[i][j]= memo(s,i+1,j-1);
+        
+//         return dp[i][j]= 1+Math.min(memo(s,i+1,j),memo(s,i,j-1));
+//     }
+    
+    int [][]dp;
+    public int lps(String s,int i,int j){
+        if(i==j)return 1;
+        if(i>j)return 0;
+        
         if(dp[i][j]!=-1)return dp[i][j];
-        
-        if(s.charAt(i)==s.charAt(j))return dp[i][j]= memo(s,i+1,j-1);
-        
-        return dp[i][j]= 1+Math.min(memo(s,i+1,j),memo(s,i,j-1));
+        if(s.charAt(i)==s.charAt(j))return dp[i][j]=2+lps(s,i+1,j-1);
+        return dp[i][j]=Math.max(lps(s,i+1,j),lps(s,i,j-1));
     }
     
     public int minInsertions(String s) {
@@ -15,6 +25,6 @@ class Solution {
         dp=new int[n][n];
         for(int []i:dp)
             Arrays.fill(i,-1);
-        return memo(s,0,n-1);
+        return n-lps(s,0,n-1);
     }
 }
