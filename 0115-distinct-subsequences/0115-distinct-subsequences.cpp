bc4164b4d6ca2b3a5f6a7ba09d8a258f2cff98d4
Singhal-Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2023-04-28 00:22:50 +0530

    Time: 62 ms (31.42%), Space: 18.5 MB (59.55%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
-    int dp[1001][1001];
-    int recur(string s,string t,int i,int j){
-        if(j==t.size())return 1;
-        if(i==s.size())return 0;
+//     int dp[1001][1001];
+//     int recur(string s,string t,int i,int j){
+//         if(j==t.size())return 1;
+//         if(i==s.size())return 0;
         
-        if(dp[i][j]!=-1)return dp[i][j];
+//         if(dp[i][j]!=-1)return dp[i][j];
         
-        if(s[i]==t[j])
-            return dp[i][j]= recur(s,t,i+1,j+1)+recur(s,t,i+1,j);
-        return dp[i][j]= recur(s,t,i+1,j);
-    }
+//         if(s[i]==t[j])
+//             return dp[i][j]= recur(s,t,i+1,j+1)+recur(s,t,i+1,j);
+//         return dp[i][j]= recur(s,t,i+1,j);
+//     }
     
     int numDistinct(string s, string t) {
-        memset(dp,-1,sizeof(dp));
-        return recur(s,t,0,0);
+        // memset(dp,-1,sizeof(dp));
+        // return recur(s,t,0,0);
+        
+        int m=s.size(),n=t.size();
+        vector<vector<unsigned >>dp(m+1,vector<unsigned>(n+1));
+        for(int i=0;i<=m;i++)
+            dp[i][n]=1;
+        
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(s[i]==t[j])
+                    dp[i][j]=dp[i+1][j]+dp[i+1][j+1];
+                else dp[i][j]=dp[i+1][j];
+                dp[i][j]%=INT_MAX;
+            }
+        }
+        
+        return dp[0][0];
     }
 };
