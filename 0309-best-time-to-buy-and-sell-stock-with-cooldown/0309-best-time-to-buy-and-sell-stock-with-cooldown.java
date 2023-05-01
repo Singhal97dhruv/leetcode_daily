cdb60d798a56c0ba2bccd3eb7107fc3de0ddb28c
Singhal-Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2023-05-02 01:51:36 +0530

    Time: 1 ms (84.85%), Space: 41.7 MB (39.89%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -0,0 +1,24 @@
+class Solution {
+    int [][]dp;
+    public int memo(int []prices,int i,int buy){
+        if(i>=prices.length)return 0;
+        
+        if(dp[i][buy]!=-1)return dp[i][buy];
+        
+        if(buy==1){
+            return dp[i][buy]= Math.max(-prices[i]+memo(prices,i+1,0),memo(prices,i+1,1));
+        }
+        else{
+            return dp[i][buy]= Math.max(prices[i]+memo(prices,i+2,1),memo(prices,i+1,0));
+        }
+        
+    }
+    
+    public int maxProfit(int[] prices) {
+        int n=prices.length;
+        dp=new int[n+1][2];
+        for(int []i:dp)
+            Arrays.fill(i,-1);
+        return memo(prices,0,1);
+    }
+}
