2f6da8b440b88463cd3f4b713c5c3f908440751c
Singhal-Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2023-05-03 00:38:35 +0530

    Time: 2 ms (87.97%), Space: 40.7 MB (85.46%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,21 +1,38 @@
 class Solution {
-    int [][][]dp;
+//     int [][][]dp;
     
-    public int memo(int[] prices,int i,int buy,int k){
-        if(i==prices.length || k<0)return 0;
-        if(dp[i][buy][k]!=-1)return dp[i][buy][k];
+//     public int memo(int[] prices,int i,int buy,int k){
+//         if(i==prices.length || k<0)return 0;
+//         if(dp[i][buy][k]!=-1)return dp[i][buy][k];
         
-        if(buy==1)
-            return dp[i][buy][k]= Math.max(-prices[i]+memo(prices,i+1,0,k-1),memo(prices,i+1,1,k));
-        else
-            return dp[i][buy][k]= Math.max(prices[i]+memo(prices,i+1,1,k),memo(prices,i+1,0,k));
-    }
+//         if(buy==1)
+//             return dp[i][buy][k]= Math.max(-prices[i]+memo(prices,i+1,0,k-1),memo(prices,i+1,1,k));
+//         else
+//             return dp[i][buy][k]= Math.max(prices[i]+memo(prices,i+1,1,k),memo(prices,i+1,0,k));
+//     }
+    
+    
     public int maxProfit(int k, int[] prices) {
         int n=prices.length;
-        dp=new int[n+1][2][k+1];
-        for(int [][]i:dp)
-            for(int []j:i)
-                Arrays.fill(j,-1);
-        return memo(prices,0,1,k);
+        // dp=new int[n+1][2][k+1];
+        // for(int [][]i:dp)
+        //     for(int []j:i)
+        //         Arrays.fill(j,-1);
+        // return memo(prices,0,1,k);
+        
+        int [][]curr=new int[2][k+1],next=new int[2][k+1];
+        for(int i=n-1;i>=0;i--){
+            for(int buy=0;buy<=1;buy++){
+                for(int j=1;j<=k;j++){
+                    if(buy==1)
+                    curr[buy][j]=Math.max(-prices[i]+next[0][j],next[1][j]);
+                    else
+                        curr[buy][j]=Math.max(prices[i]+next[1][j-1],next[0][j]);
+                }
+            }
+            next=curr;
+        }
+        
+        return curr[1][k];
     }
 }
