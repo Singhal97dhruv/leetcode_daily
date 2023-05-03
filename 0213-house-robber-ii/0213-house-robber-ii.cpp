317e5abe97da1b8254ae1109cba11bd521dfad3b
Singhal-Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2023-05-03 22:40:05 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (97.97%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
-    int dp[101][2];
-    int memo(vector<int>&nums,int i,bool firstTaken){
-        if(i>=nums.size() || (i==nums.size()-1 && firstTaken))return 0;
-        if(dp[i][firstTaken]!=-1)return dp[i][firstTaken];
-        if(i==0)return dp[i][firstTaken]= max(nums[i]+memo(nums,i+2,true),memo(nums,i+1,false));
+    int dp[102][2];
+//     int memo(vector<int>&nums,int i,bool firstTaken){
+//         if(i>=nums.size() || (i==nums.size()-1 && firstTaken))return 0;
+//         if(dp[i][firstTaken]!=-1)return dp[i][firstTaken];
+//         if(i==0)return dp[i][firstTaken]= max(nums[i]+memo(nums,i+2,true),memo(nums,i+1,false));
         
-        return dp[i][firstTaken]= max(nums[i]+memo(nums,i+2,firstTaken),memo(nums,i+1,firstTaken));
+//         return dp[i][firstTaken]= max(nums[i]+memo(nums,i+2,firstTaken),memo(nums,i+1,firstTaken));
         
-    }
+//     }
     
     int rob(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
-        return memo(nums,0,false);
+        // memset(dp,-1,sizeof(dp));
+        // return memo(nums,0,false);
+        int n=nums.size();
+        dp[n-1][0]=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            if(i==0)dp[i][0]=dp[i+1][0];
+            else dp[i][0]=max(nums[i]+dp[i+2][0],dp[i+1][0]);
+            dp[i][1]=max(nums[i]+dp[i+2][1],dp[i+1][1]);
+        }
+        return max(dp[0][1],dp[0][0]);
     }
 };
