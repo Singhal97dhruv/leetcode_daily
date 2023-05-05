4a6098b9388cf7478c537bcbf941084676237a55
Singhal-Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2023-05-05 13:41:02 +0530

    Time: 10 ms (26.50%), Space: 43.1 MB (31.60%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,19 +1,24 @@
 class Solution {
-    int [][][]dp;
-    public int memo(int[]piles,int st,int end,int Alice){
-        if(st>end)return 0;
-        if(dp[st][end][Alice]!=-1)return dp[st][end][Alice];
-        if(Alice==1){
-            return dp[st][end][Alice]= Math.max(piles[st]+memo(piles,st+1,end,0),piles[end]+memo(piles,st,end-1,0));
-        }
-        else 
-            return dp[st][end][Alice]= Math.min(-piles[st]+memo(piles,st+1,end,1),-piles[end]+memo(piles,st,end-1,1));
-    }
     public boolean stoneGame(int[] piles) {
-        dp=new int[piles.length][piles.length][2];
-        for(int [][]i:dp)
-            for(int []j:i)
-                Arrays.fill(j,-1);
-        return memo(piles,0,piles.length-1,1)>0?true:false;
+        int n=piles.length;
+        int dp[][]=new int[n][n];
+        // int sum=iota(piles.begin(),piles.end(),0);
+        int sum=0;
+            for(int i:piles)sum+=i;
+        // for(int i=0;i<n;i++)dp[i][i]=piles[i];
+        for(int d=0;d<n;d++){
+            for(int i=0,j=d;j<n;i++,j++){
+                if(d==0){
+                    dp[i][j]=piles[i];
+                }
+                else if(d==1){
+                    dp[i][j]=Math.max(piles[i],piles[j]);
+                }
+                else{
+                    dp[i][j]=Math.max(piles[i]+Math.min(dp[i+2][j],dp[i+1][j-1]),piles[j]+Math.min(dp[i+1][j-1],dp[i][j-2]));
+                }
+            }
+        }
+        return dp[0][n-1]*2>sum?true:false;
     }
 }
