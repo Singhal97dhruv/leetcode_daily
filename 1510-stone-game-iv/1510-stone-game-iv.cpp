48d24b48b043359cd54fa24de3a72a716dfe970d
Singhal-Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2023-05-12 00:37:07 +0530

    Time: 34 ms (92.26%), Space: 6.2 MB (97.02%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -3,11 +3,21 @@ public:
     bool dp[100001];
     bool winnerSquareGame(int n) {
         
-        if(n==0)return 0;
-        if(dp[n]==true)return dp[n];
-        for(int i=1;i*i<=n;i++){
-            if(winnerSquareGame(n-i*i)==0)return dp[n]=true;
+        // if(n==0)return 0;
+        // if(dp[n]==true)return dp[n];
+        // for(int i=1;i*i<=n;i++){
+        //     if(winnerSquareGame(n-i*i)==0)return dp[n]=true;
+        // }
+        // return false;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j*j<=i;j++){
+                if(!dp[i-j*j]){
+                    dp[i]=true;
+                    break;
+                }
+            }
         }
-        return false;
+        return dp[n];
     }
 };
