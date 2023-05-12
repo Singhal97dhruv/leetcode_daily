04485623599657ef58c701fabd7dc140126bccae
Singhal-Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2023-05-13 00:42:29 +0530

    Time: 392 ms (33.48%), Space: 112.8 MB (97.98%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
-    long long dp[100001]={[0 ... 100000]=-1};
-    long long recur(int idx,vector<vector<int>>&q){
-        if(idx>=q.size())return 0;
-        if(dp[idx]!=-1)return dp[idx];
+//     long long dp[100001]={[0 ... 100000]=-1};
+//     long long recur(int idx,vector<vector<int>>&q){
+//         if(idx>=q.size())return 0;
+//         if(dp[idx]!=-1)return dp[idx];
         
-        return dp[idx]= max(q[idx][0]+recur(idx+q[idx][1]+1,q),recur(idx+1,q));
+//         return dp[idx]= max(q[idx][0]+recur(idx+q[idx][1]+1,q),recur(idx+1,q));
         
-    }
+//     }
     
     long long mostPoints(vector<vector<int>>& questions) {
-        return recur(0,questions);
+        // return recur(0,questions);
+        
+        int n=questions.size();
+        
+        long long dp[100001]={0};
+        
+        for(int i=n-1;i>=0;i--){
+            
+            dp[i]=max(questions[i][0]+dp[(i+questions[i][1]+1)<n?(i+questions[i][1]+1):n],
+                     dp[i+1]);
+        }
+        return dp[0];
     }
 };
