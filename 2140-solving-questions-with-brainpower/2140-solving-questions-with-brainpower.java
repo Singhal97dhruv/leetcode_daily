c74b5be3f8d134aefcd690b062235df9e2379ab7
Singhal-Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2023-05-13 00:52:24 +0530

    Time: 6 ms (88.36%), Space: 92.9 MB (94.71%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
@@ -8,7 +8,13 @@ class Solution {
     public long mostPoints(int[][] questions) {
         int n=questions.length;
         dp=new long[n+1];
-        Arrays.fill(dp,-1);
-        return memo(0,questions);
-    }
+        // Arrays.fill(dp,-1);
+        // return memo(0,questions);
+        for(int i=n-1;i>=0;i--){
+            dp[i]=Math.max(questions[i][0]+dp[(i+questions[i][1]+1)<n?(i+questions[i][1]+1):n],dp[i+1]);
+        }
+        
+        return dp[0];
+
+        }
 }
