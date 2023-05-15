19a4fcf2b4e02e0e5dae807dcff20f805429e3a5
Singhal-Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2023-05-16 02:16:25 +0530

    Time: 3 ms (93.46%), Space: 10.4 MB (52.80%) - LeetHub

--- 0789-escape-the-ghosts/0789-escape-the-ghosts.cpp
+++ 0789-escape-the-ghosts/0789-escape-the-ghosts.cpp
@@ -1,11 +1,10 @@
 class Solution {
-    public boolean escapeGhosts(int[][] ghosts, int[] target) {
-        
-        int distFromMe= Math.abs(target[0])+Math.abs(target[1]);
-        
-        for(int []i:ghosts){
-            if(distFromMe>=Math.abs(i[0]-target[0])+Math.abs(i[1]-target[1]))return false;
+public:
+    bool escapeGhosts(vector<vector<int>>& ghosts, vector<int>& target) {
+        int mydist=abs(target[0])+abs(target[1]);
+        for(auto &i:ghosts){
+            if(mydist>=abs(i[0]-target[0])+abs(i[1]-target[1]))return false;
         }
         return true;
     }
-}
+};
