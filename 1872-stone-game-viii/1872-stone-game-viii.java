08308bfae6253e97a1feaca34cca3104d597b58c
Singhal-Saab <91372434+Singhal97dhruv@users.noreply.github.com>
2023-05-22 23:00:53 +0530

    Time: 5 ms (77.78%), Space: 56.8 MB (30.56%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.java
+++ 1872-stone-game-viii/1872-stone-game-viii.java
@@ -13,7 +13,14 @@ class Solution {
         int n=stones.length;
         for(int i=1;i<n;i++)stones[i]+=stones[i-1];
         dp=new int[n];
-        Arrays.fill(dp,Integer.MIN_VALUE);
-        return memo(stones,1);
+        // Arrays.fill(dp,Integer.MIN_VALUE);
+        // return memo(stones,1);
+        
+        //Bottom UP Approach
+        dp[n-1]=stones[n-1];
+        for(int i=n-2;i>0;i--){
+            dp[i]=Math.max(stones[i]-dp[i+1],dp[i+1]);
+        }
+        return dp[1];
     }
 }
