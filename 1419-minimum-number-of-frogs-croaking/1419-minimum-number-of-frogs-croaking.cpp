da281cc082935b46afba8d8db6840a51dde053fe
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-05-26 13:18:34 +0530

    Time: 39 ms (52.22%), Space: 9.2 MB (38.54%) - LeetHub

--- 1419-minimum-number-of-frogs-croaking/1419-minimum-number-of-frogs-croaking.cpp
+++ 1419-minimum-number-of-frogs-croaking/1419-minimum-number-of-frogs-croaking.cpp
@@ -1,36 +1,53 @@
 class Solution {
 public:
     int minNumberOfFrogs(string croakOfFrogs) {
-        int ptr=0,ans=0;
-        int c,r,o,a,k;
-        c=r=o=a=k=0;
-        for(char d:croakOfFrogs){
+//         int ptr=0,ans=0;
+//         int c,r,o,a,k;
+//         c=r=o=a=k=0;
+//         for(char d:croakOfFrogs){
             
-            switch(d){
-                case 'c':
-                    ptr++;
-                    c++;
-                    break;
-                case 'r':
-                    r++;
-                    break;
-                case 'o':
-                    o++;
-                    break;
-                case 'a':
-                    a++;
-                    break;
-                case 'k':
-                    ptr--;
-                    k++;
-                    break;
+//             switch(d){
+//                 case 'c':
+//                     ptr++;
+//                     c++;
+//                     break;
+//                 case 'r':
+//                     r++;
+//                     break;
+//                 case 'o':
+//                     o++;
+//                     break;
+//                 case 'a':
+//                     a++;
+//                     break;
+//                 case 'k':
+//                     ptr--;
+//                     k++;
+//                     break;
                     
+//             }
+//             if(c<r || r<o || o<a || a<k)return -1;
+//             ans=max(ans,ptr);
+//         }
+//         if(ptr==0 && c==r && c==o && o==a && a==k)return ans;
+//         return -1;
+        
+        int arr[5]={0};
+        int frog=0,total=0;
+        for(char ch:croakOfFrogs){
+            
+            int n=string("croak").find(ch);
+            
+            arr[n]++;
+            if(n==0){
+                total=max(total,++frog);
+                
             }
-            if(c<r || r<o || o<a || a<k)return -1;
-            ans=max(ans,ptr);
+            else if(--arr[n-1]<0)return -1;
+            else if(n==4)frog--;
+            
         }
-        if(ptr==0 && c==r && c==o && o==a && a==k)return ans;
-        return -1;
+        return frog==0?total:-1;
         
         
         
