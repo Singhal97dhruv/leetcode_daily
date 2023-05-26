60056fb276deea4816a29d02f4e1ef49219b4fcc
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-05-26 21:57:29 +0530

    Time: 6 ms (85.10%), Space: 40.9 MB (99.52%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
@@ -2,26 +2,38 @@ class Solution {
     
     int []dp;
     
-    public int memo(int []arr,int idx,int k){
-        if(idx>=arr.length)return 0;
-        if(dp[idx]!=-1)return dp[idx];
-        int ans=0,mx=Integer.MIN_VALUE;
-        for(int i=0;i<k;i++){
-            if (idx+i>=arr.length)break;
-            mx=Math.max(mx,arr[idx+i]);
+//     public int memo(int []arr,int idx,int k){
+//         if(idx>=arr.length)return 0;
+//         if(dp[idx]!=-1)return dp[idx];
+//         int ans=0,mx=Integer.MIN_VALUE;
+//         for(int i=0;i<k;i++){
+//             if (idx+i>=arr.length)break;
+//             mx=Math.max(mx,arr[idx+i]);
             
-            int leftSub= (i+1)*mx;
-            int rightSub= memo(arr,idx+i+1,k);
-            ans=Math.max(ans,leftSub+rightSub);
-        }
-        return dp[idx]= ans;
+//             int leftSub= (i+1)*mx;
+//             int rightSub= memo(arr,idx+i+1,k);
+//             ans=Math.max(ans,leftSub+rightSub);
+//         }
+//         return dp[idx]= ans;
         
-    }
+//     }
     
     public int maxSumAfterPartitioning(int[] arr, int k) {
         int n=arr.length;
         dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        return memo(arr,0,k);
+        // Arrays.fill(dp,-1);
+        // return memo(arr,0,k);
+        
+        for(int i=n-1;i>=0;i--){
+            int mx=0;
+            for(int j=0;j<k;j++){
+                if(i+j>=n)break;
+                mx=Math.max(mx,arr[i+j]);
+                int leftSub=(j+1)*mx;
+                int rightSub=dp[i+j+1];
+                dp[i]=Math.max(dp[i],leftSub+rightSub);
+            }
+        }
+        return dp[0];
     }
 }
