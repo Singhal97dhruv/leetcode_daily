81d717906fe234c6b7e5e885919775231e33ae71
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-05-28 01:30:48 +0530

    Time: 55 ms (33.33%), Space: 44.3 MB (33.33%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
@@ -2,35 +2,44 @@ class Solution {
     
     int []dp;
     Set<String>set;  
-    public int memo(String s,int idx){
-        int n=s.length();
-        if(idx>=n)return 0;
+//     public int memo(String s,int idx){
+//         int n=s.length();
+//         if(idx>=n)return 0;
         
-        if(dp[idx]!=-1)return dp[idx];
+//         if(dp[idx]!=-1)return dp[idx];
     
-        int ans=memo(s,idx+1);
+//         int ans=memo(s,idx+1);
         
-        for(int i=1;i+idx<=n;i++)
-        {
-            if(set.contains(s.substring(idx,idx+i))){
-                ans=Math.max(ans,i+memo(s,idx+i));
-            }
-        }
-        return dp[idx]=ans;
+//         for(int i=1;i+idx<=n;i++)
+//         {
+//             if(set.contains(s.substring(idx,idx+i))){
+//                 ans=Math.max(ans,i+memo(s,idx+i));
+//             }
+//         }
+//         return dp[idx]=ans;
         
-    }
+//     }
     
     public int minExtraChar(String s, String[] dictionary) {
         
         int n=s.length();
         dp=new int[n+1];
-        Arrays.fill(dp,-1);
+        // Arrays.fill(dp,-1);
         set=new HashSet<>();
         for(String st:dictionary){
             set.add(st);
         }
         
-        return n-memo(s,0);
+        // return n-memo(s,0);
+        
+        for(int i=n-1;i>=0;i--){
+            dp[i]=dp[i+1];
+            for(int j=1;j+i<=n;j++){
+                if(set.contains(s.substring(i,i+j)))
+                    dp[i]=Math.max(dp[i],j+dp[i+j]);
+            }
+        } 
+        return n-dp[0];
         
     }
 }
