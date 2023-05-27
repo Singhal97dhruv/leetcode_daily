ba2cd48ef26105eb3f48ceb847d4394af7e8d21a
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-05-28 01:11:06 +0530

    Time: 234 ms (62.50%), Space: 81.5 MB (62.50%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -1,27 +1,40 @@
 class Solution {
 public:
-    int dp[51]={[0 ...50]=-1};
-    int recur(string s,set<string>&set,int idx){
-        if(idx>=s.size())return 0;
-        int ans=0;
-        if(dp[idx]!=-1)return dp[idx];
-        ans=recur(s,set,idx+1);
+    int dp[52]={[0 ...51]=0};
+//     int recur(string s,set<string>&set,int idx){
+//         if(idx>=s.size())return 0;
+//         int ans=0;
+//         if(dp[idx]!=-1)return dp[idx];
+//         ans=recur(s,set,idx+1);
 
-        for(int i=0;i+idx<s.size();i++){
-            if(set.find(s.substr(idx,i+1))!=set.end()){
-                ans=max(i+1+recur(s,set,idx+i+1),ans);
+//         for(int i=0;i+idx<s.size();i++){
+//             if(set.find(s.substr(idx,i+1))!=set.end()){
+//                 ans=max(i+1+recur(s,set,idx+i+1),ans);
                 
-            }
+//             }
            
-        }
-        return dp[idx]= ans;
-    }
+//         }
+//         return dp[idx]= ans;
+//     }
     
     int minExtraChar(string s, vector<string>& dictionary) {
         set<string>set;
         for(string s:dictionary){
             set.insert(s);
         }
-        return s.size()- recur(s,set,0);
+        // return s.size()- recur(s,set,0);
+        int n=s.size();
+        for(int i=n-1;i>=0;i--){
+            dp[i]=dp[i+1];
+            for(int j=1;i+j<=s.size();j++){
+                if(set.find(s.substr(i,j))!=set.end()){
+                    dp[i]=max(dp[i],j+dp[i+j]);
+                }
+            }
+        }
+        
+        
+        
+        return n-dp[0];
     }
 };
