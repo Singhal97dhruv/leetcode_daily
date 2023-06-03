b6e1e02075ec43ffce7f9d08320d8024e9cd4212
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-06-03 15:32:01 +0530

    Time: 11 ms (67.67%), Space: 5.9 MB (86.65%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    
+    /// 1st approach
     void recur(string T,string S,unordered_map<int,int>&map,unordered_set<string>& set){
         
         for(int i=0;i<T.size();i++){
@@ -20,11 +20,29 @@ public:
         
     }
     
+    // 2nd Approach
+    int dfs(vector<int>&arr){
+        int sum=0;
+        for(int i=0;i<26;i++){
+            if(arr[i]==0)continue;
+            sum++;
+            arr[i]--;
+            sum+=dfs(arr);
+            arr[i]++;
+        }
+        return sum;
+    }
+    
     
     int numTilePossibilities(string tiles) {
-        unordered_map<int,int>map;
-        unordered_set<string>set;
-        recur(tiles,"",map,set);
-        return set.size();
+        // unordered_map<int,int>map;
+        // unordered_set<string>set;
+        // recur(tiles,"",map,set);
+        // return set.size();
+        
+        vector<int>arr(26,0);
+        for(char c:tiles)arr[c-'A']++;
+        return dfs(arr);
+        
     }
 };
