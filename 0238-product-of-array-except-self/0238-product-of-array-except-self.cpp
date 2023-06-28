6da51625b303a4055f4f143db18e65e9c9f86534
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-06-28 23:52:19 +0530

    Time: 28 ms (64.88%), Space: 25.8 MB (9.18%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int>res(nums.size(),1);
+//         vector<int>res(nums.size(),1);
         
-        int curr=1;
-        for(int i=0;i<nums.size();i++){
-            res[i]*=curr;
-            curr*=nums[i];
-        }
-        curr=1;
-        for(int i=nums.size()-1;i>=0;i--){
-            res[i]*=curr;
-            curr*=nums[i];
+//         int curr=1;
+//         for(int i=0;i<nums.size();i++){
+//             res[i]*=curr;
+//             curr*=nums[i];
+//         }
+//         curr=1;
+//         for(int i=nums.size()-1;i>=0;i--){
+//             res[i]*=curr;
+//             curr*=nums[i];
+//         }
+//         return res;
+        int n=nums.size();
+        vector<int>pref,suff;
+        pref=nums;
+        suff=nums;
+        for(int i=1;i<n;i++)pref[i]*=pref[i-1];
+        for(int i=n-2;i>=0;i--)suff[i]*=suff[i+1];
+        
+        vector<int>res;
+        for(int i=0;i<n;i++){
+            if(i==0)res.push_back(suff[i+1]);
+            else if(i==n-1)res.push_back(pref[i-1]);
+            else res.push_back(pref[i-1]*suff[i+1]);
         }
         return res;
-        
     }
 };
