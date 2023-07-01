4a93770a2653d1c441ec4311320bd51478f2ce54
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-01 23:45:46 +0530

    Time: 7 ms (48.79%), Space: 7.2 MB (99.72%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        // string str="";
+        // for(char c:s){
+        //     if((c>=97 && c<=122) ||(c>=48 && c<=57))str+=c;
+        //     else if(c>=65 && c<=90)str+=tolower(c);
+        // }
+        // int n=str.size();
+        // // if(n<3)return true;
+        // for(int i=0;i<n/2;i++){
+        //     if(str[i]!=str[n-i-1])return false;
+        // }
+        // return true;
+        
+        int i=0,j=s.size()-1;
+        
+       while(i<j){
+           while(i<j && !isalnum(s[i]))i++;
+           if(i==j)break;
+           while(i<j && !isalnum(s[j]))j--;
+           if(i==j)break;
+           char sI=isalpha(s[i]) ? tolower(s[i]) : s[i];
+           char sJ=isalpha(s[j]) ? tolower(s[j]) : s[j];
+           if(sI==sJ)i++,j--;
+           else return false;
+           
+           
+           
+           
+       }
+        return true;
+        
+        
+        
+    }
+};
