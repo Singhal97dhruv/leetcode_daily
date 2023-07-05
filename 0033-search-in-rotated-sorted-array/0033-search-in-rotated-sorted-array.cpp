9a3abaf0288d1f1603fde903902328ca60d33e4f
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-05 23:43:21 +0530

    Time: 7 ms (31.30%), Space: 11 MB (94.34%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -29,23 +29,54 @@ public:
 //         }
 //         return -1;
         
-        int l=0,r=nums.size()-1;
-        while(l<r){
-            int mid=l+(r-l)/2;
+        // int l=0,r=nums.size()-1;
+        // while(l<r){
+        //     int mid=l+(r-l)/2;
+        //     if(nums[mid]==target)return mid;
+        //     else if(nums[l]>nums[mid]){
+        //         if(target>nums[mid] && target<=nums[r])l=mid+1;
+        //         else r=mid;
+        //     }
+        //     else if(nums[mid]>nums[r]){
+        //         if(target>=nums[l] && target<nums[mid])r=mid;
+        //         else l=mid+1;
+        //     }
+        //     else {
+        //         if(target>=nums[l] && target<=nums[mid])r=mid;
+        //         else l=mid+1;
+        //     }
+        // }
+        // return nums[l]==target?l:-1;
+        
+        
+        int left=0,right=nums.size()-1;
+        
+        while(left<right){
+            
+            int mid=left+(right-left)/2;
+            
             if(nums[mid]==target)return mid;
-            else if(nums[l]>nums[mid]){
-                if(target>nums[mid] && target<=nums[r])l=mid+1;
-                else r=mid;
+            
+            if(nums[left]>nums[mid]){
+                
+                if(target<nums[left] && target>nums[mid])left=mid+1;
+                else right=mid;
+                
             }
-            else if(nums[mid]>nums[r]){
-                if(target>=nums[l] && target<nums[mid])r=mid;
-                else l=mid+1;
+
+            else if(nums[right]<nums[mid]){
+                if(target<nums[mid] && target>=nums[left])right=mid;
+                else left=mid+1;
             }
+            
             else {
-                if(target>=nums[l] && target<=nums[mid])r=mid;
-                else l=mid+1;
+                if(target<nums[mid])right=mid;
+                else left=mid+1;
             }
         }
-        return nums[l]==target?l:-1;
+        
+        return nums[left]==target?left:-1;
+        
+        
     }
 };
