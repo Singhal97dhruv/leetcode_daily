c7124aaaf9db452d0f2d9a64dac919bf873a7c6a
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-06 22:49:59 +0530

    Time: 12 ms (92.01%), Space: 13.4 MB (14.68%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    
+    void settingZeros(vector<vector<int>>&mat,int ii,int jj){
+        for(int i=0;i<mat.size();i++){
+            mat[i][jj]=0;
+        }
+        for(int j=0;j<mat[0].size();j++){
+            mat[ii][j]=0;
+        }
+    }
+    
+    void setZeroes(vector<vector<int>>& matrix) {
+        
+//         bool row=false,col=false;
+//         for(int i=0;i<matrix.size();i++){
+//             if(matrix[i][0]==0){
+//                 row=true;
+//                 break;
+//             }
+//         }
+//         for(int i=0;i<matrix[0].size();i++){
+//             if(matrix[0][i]==0){
+//                 col=true;
+//                 break;
+//             }
+//         }
+//         for(int i=1;i<matrix.size();i++){
+//             for(int j=1;j<matrix[0].size();j++){
+//                 if(matrix[i][j]==0){
+//                     matrix[i][0]=0;
+//                     matrix[0][j]=0;
+//                 }
+//             }
+//         }
+//         for(int i=1;i<matrix.size();i++){
+//             for(int j=1;j<matrix[0].size();j++){
+//                     if(matrix[i][0]==0 || matrix[0][j]==0){
+//                         matrix[i][j]=0;
+//                 }
+//             }
+//         }
+//         if(row)
+//         for(int i=0;i<matrix.size();i++){
+//             matrix[i][0]=0;
+            
+//         }
+//         if(col)
+//             for(int j=0;j<matrix[0].size();j++){
+//                 matrix[0][j]=0;
+//             }
+    
+    vector<pair<int,int>>set;
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                if(matrix[i][j]==0)set.push_back({i,j});
+            }
+        }
+        
+        for(auto p:set){
+            auto [i,j]=p;
+            settingZeros(matrix,i,j);
+        }
+        
+        return;
+    
+    }
+};
