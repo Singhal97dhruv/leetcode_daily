48139df11e3af205abd63a5c12ffd55f77894b3e
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-10 00:42:03 +0530

    Time: 4 ms (80.07%), Space: 13.8 MB (10.10%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -20,26 +20,44 @@ public:
     //    traverse(tree->right,i+1);  
     //  }
     
+    vector<vector<int>>res;
+    
+    void traverse(TreeNode*root,int lev){
+        if(!root)return ;
+        if(lev==res.size())res.resize(lev+1);
+        res[lev].push_back(root->val);
+        traverse(root->left,lev+1);
+        traverse(root->right,lev+1);
+        
+    }
+    
+    
+    
     vector<vector<int>> levelOrder(TreeNode* root) {
 //         // vect.resize(3);
 //         traverse(root,0);
         
 //         return vect;
+        
+        //ITERATION APPROACH BY SINGHAL SAAB
+//         if(!root)return {};
+//         vector<vector<int>>res;
+//         queue<TreeNode*>q;
+//         q.push(root);
+//         while(!q.empty()){
+//             int n=q.size();
+//             vector<int>temp;
+//             for(int i=0;i<n;i++){
+//                 TreeNode* top=q.front();
+//                 temp.push_back(top->val);q.pop();
+//                 if(top->left)q.push(top->left);
+//                 if(top->right)q.push(top->right);
+//             }
+//             res.push_back(temp);
+//         }
+//         return res;
         if(!root)return {};
-        vector<vector<int>>res;
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-            int n=q.size();
-            vector<int>temp;
-            for(int i=0;i<n;i++){
-                TreeNode* top=q.front();
-                temp.push_back(top->val);q.pop();
-                if(top->left)q.push(top->left);
-                if(top->right)q.push(top->right);
-            }
-            res.push_back(temp);
-        }
+        traverse (root,0);
         return res;
         
         
