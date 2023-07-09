be6fe7170fc9816682eeea68acd4cd897c1b9f01
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-09 23:30:06 +0530

    Time: 32 ms (71.34%), Space: 23.2 MB (54.65%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -20,14 +20,36 @@ public:
         // return root;
 
         
-        if(root->val>p->val && root->val>q->val){
-            return lowestCommonAncestor(root->left,p,q);
-        }
-        if(root->val<p->val && root->val<q->val){
-            return lowestCommonAncestor(root->right,p,q);
-        }
-        return root;
+        // if(root->val>p->val && root->val>q->val){
+        //     return lowestCommonAncestor(root->left,p,q);
+        // }
+        // if(root->val<p->val && root->val<q->val){
+        //     return lowestCommonAncestor(root->right,p,q);
+        // }
+        // return root;
         
-    
+        
+        queue<TreeNode*>qq;
+        qq.push(root);
+        while(!qq.empty()){
+            
+            int n=qq.size();
+            for(int i=0;i<n;i++){
+
+                TreeNode*t=qq.front();qq.pop();
+
+                if(t->val>p->val && t->val>q->val){
+                    qq.push(t->left);
+                }
+                else if(t->val<p->val && t->val<q->val){
+                    qq.push(t->right);
+                }
+                else return t;
+            }
+            
+            
+        }
+        
+    return nullptr;
     }
 };
