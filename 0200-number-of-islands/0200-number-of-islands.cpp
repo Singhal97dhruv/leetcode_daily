c7fe83c7b3ed6c320b69f2d228d8aed69c31cc59
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-11 00:45:23 +0530

    Time: 48 ms (32.86%), Space: 18.1 MB (29.89%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,26 +1,61 @@
 class Solution {
 public:
     vector<int>offset{0,1,0,-1,0};
-    void islandToWater(vector<vector<char>>&grid,int i,int j){
-        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]=='0')return;
-        grid[i][j]='0';
-        for(int k=0;k<4;k++){
-            islandToWater(grid,i+offset[k],j+offset[k+1]);
-        }
+//     void islandToWater(vector<vector<char>>&grid,int i,int j){
+//         if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]=='0')return;
+//         grid[i][j]='0';
+//         for(int k=0;k<4;k++){
+//             islandToWater(grid,i+offset[k],j+offset[k+1]);
+//         }
         
-    }
+//     }
     
     int numIslands(vector<vector<char>>& grid) {
         
+        // int cnt=0;
+        // for(int i=0;i<grid.size();i++){
+        //     for(int j=0;j<grid[0].size();j++){
+        //         if(grid[i][j]=='1'){
+        //             cnt++;
+        //             islandToWater(grid,i,j);
+        //         }
+        //     }
+        // }
+        // return cnt;
+        
         int cnt=0;
+        
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j]=='1'){
                     cnt++;
-                    islandToWater(grid,i,j);
+                    
+                    queue<pair<int,int>>q;
+                    q.push({i,j});
+                    
+                    while(!q.empty()){
+                        auto [s,t]=q.front();q.pop();
+                        
+                        grid[s][t]='0';
+                        
+                        for(int k=0;k<4;k++){
+                            int r=s+offset[k],c=t+offset[k+1];
+                            if(r<0 || c<0 || r>=grid.size() || c>=grid[0].size() || grid[r][c]=='0')continue;
+                            grid[r][c]='0';
+                            q.push({r,c});
+                        }
+                        
+                    }
+                    
                 }
             }
         }
-        return cnt;
+        
+      return cnt;  
+        
+        
+        
+        
+        
     }
 };
