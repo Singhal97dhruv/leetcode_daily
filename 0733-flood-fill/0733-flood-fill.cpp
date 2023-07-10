a84b99deb298d2ac383b6da4d0dac031f55193a8
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-11 00:55:12 +0530

    Time: 14 ms (29.38%), Space: 14 MB (63.94%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+//     void dfs(vector<vector<int>>&image,int ii,int jj,int color,int ijcolor){
+//         if(ii<0 || ii>=image.size() || jj<0 || jj>=image[0].size() || image[ii][jj]==color || image[ii][jj]!=ijcolor)return;
+        
+//         image[ii][jj]=color;
+//         dfs(image,ii+1,jj,color,ijcolor);
+//         dfs(image,ii,jj+1,color,ijcolor);
+//         dfs(image,ii,jj-1,color,ijcolor);
+//         dfs(image,ii-1,jj,color,ijcolor);
+
+//     }
+    
+    vector<int>offset{0,1,0,-1,0};
+    void dfs(vector<vector<int>>&img,int i,int j,int color,int colToSet){
+        if(i<0 || j<0 || i>=img.size() || j>=img[0].size() || img[i][j]==colToSet || img[i][j]!=color)return ;
+        
+        img[i][j]=colToSet;
+        for(int k=0;k<4;k++){
+            dfs(img,i+offset[k],j+offset[k+1],color,colToSet);
+        }
+        
+        
+    }
+    
+    
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        // dfs(image,sr,sc,color,image[sr][sc]);
+        // return image;
+        
+        dfs(image,sr,sc,image[sr][sc],color);
+        
+        return image;
+    }
+};
