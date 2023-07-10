55ed02e5c9f02628f04e732f32c8e9458e2b62b6
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-11 00:35:36 +0530

    Time: 41 ms (66.42%), Space: 12.2 MB (77.03%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int>offset{0,1,0,-1,0};
+    void islandToWater(vector<vector<char>>&grid,int i,int j){
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]=='0')return;
+        grid[i][j]='0';
+        for(int k=0;k<4;k++){
+            islandToWater(grid,i+offset[k],j+offset[k+1]);
+        }
+        
+    }
+    
+    int numIslands(vector<vector<char>>& grid) {
+        
+        int cnt=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]=='1'){
+                    cnt++;
+                    islandToWater(grid,i,j);
+                }
+            }
+        }
+        return cnt;
+    }
+};
