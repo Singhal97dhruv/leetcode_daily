cffd746543c398616552ad4f1069e40695fc9cf1
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-11 23:50:33 +0530

    Time: 23 ms (85.49%), Space: 13.3 MB (49.82%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -11,17 +11,36 @@
 class Solution {
 public:
     
-    ListNode* mergeTwoSortedLists(ListNode* l1,ListNode* l2){
-        if(l1==NULL)return l2;
-        else if(l2==NULL)return l1;
+//     ListNode* mergeTwoSortedLists(ListNode* l1,ListNode* l2){
+//         if(l1==NULL)return l2;
+//         else if(l2==NULL)return l1;
+//         if(l1->val<l2->val){
+//              l1->next=mergeTwoSortedLists(l1->next,l2);
+//             return l1;
+//         }
+
+//         l2->next=mergeTwoSortedLists(l1,l2->next);
+//         return l2;
+//     }
+    
+    
+    ListNode*mergeTwoSortedLists(ListNode*l1,ListNode*l2){
+        if(!l1 && !l2)return NULL;
+        if(!l1)return l2;
+        if(!l2)return l1;
+        
         if(l1->val<l2->val){
-             l1->next=mergeTwoSortedLists(l1->next,l2);
+            l1->next=mergeTwoSortedLists(l1->next,l2);
             return l1;
         }
-
-        l2->next=mergeTwoSortedLists(l1,l2->next);
-        return l2;
+                
+            l2->next=mergeTwoSortedLists(l1,l2->next);
+            return l2;
+        
+        
     }
+    
+    
     
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         
@@ -51,7 +70,18 @@ public:
 //         }
 //         return curr;
         
-        int n=lists.size();
+        // int n=lists.size();
+        // if(n==0)return NULL;
+        // while(n>1){
+        //     for(int i=0;i<n/2;i++){
+        //         lists[i]=mergeTwoSortedLists(lists[i],lists[n-i-1]);
+        //     }
+        //     n=(n+1)/2;
+        // }
+        // return lists.front();
+        
+        
+        int n =lists.size();
         if(n==0)return NULL;
         while(n>1){
             for(int i=0;i<n/2;i++){
@@ -59,7 +89,7 @@ public:
             }
             n=(n+1)/2;
         }
-        return lists.front();
         
+        return lists.front();
     }
 };
