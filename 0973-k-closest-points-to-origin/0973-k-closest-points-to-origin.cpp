265af72c7e6e45fc3c3c413633c136730413347e
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-11 23:59:06 +0530

    Time: 221 ms (86.99%), Space: 63.1 MB (80.82%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+     
+        // priority_queue<pair<int,pair<int,int>>>pq;
+        // for(int i=0;i<points.size();i++){
+        //     int x=points[i][0];
+        //     int y=points[i][1];
+        //     pq.push({x*x+y*y,{x,y}});
+        //     if(pq.size()>k)pq.pop();
+        // }
+        // vector<vector<int>>res;
+        // while(!pq.empty()){
+        //     // pair temp=pq.top().second;
+        //     res.push_back({pq.top().second.first,pq.top().second.second});
+        //     pq.pop();
+        // }
+        // return res;
+        
+        
+        priority_queue<pair<int,pair<int,int>>>nearOriginDistances;
+        
+        for(int i=0;i<points.size();i++){
+            int x=points[i][0],y=points[i][1];
+            nearOriginDistances.push({x*x + y*y,{x,y}});
+            
+            if(nearOriginDistances.size()>k)nearOriginDistances.pop();
+        }
+            
+        vector<vector<int>>res;
+        
+        while(!nearOriginDistances.empty()){
+            auto x=nearOriginDistances.top();
+            nearOriginDistances.pop();
+            res.push_back({x.second.first,x.second.second});
+        }
+        return res;
+        
+    }
+};
