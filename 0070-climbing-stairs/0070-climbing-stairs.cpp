e50499db424a5aefe6ad33fb3502de7b17910154
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-13 00:11:54 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (56.83%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -24,13 +24,24 @@ public:
     //     dypr(n,dp);
     // return dp[n];
         
-        vector<int>dp(n+1,0);
-            dp[0]=1;dp[1]=1;
-            for(int i=2;i<=n;i++){
-                dp[i]=dp[i-1]+dp[i-2];
-            }
+//         vector<int>dp(n+1,0);
+//             dp[0]=1;dp[1]=1;
+//             for(int i=2;i<=n;i++){
+//                 dp[i]=dp[i-1]+dp[i-2];
+//             }
         
-        return dp[n];
+//         return dp[n];
+        
+        if(n==1)return 1;
+        if(n==2)return 2;
+         
+        int A=1,B=1,C;
+        for(int i=2;i<=n;i++){
+            C=A+B;
+            A=B;
+            B=C;
+        }
+        return C;
         
     }
 };
