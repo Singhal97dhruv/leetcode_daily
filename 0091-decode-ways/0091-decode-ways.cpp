123074af52d181a570c05ba6b23b03ac4c9b35bd
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-13 23:07:09 +0530

    Time: 0 ms (100.00%), Space: 15.9 MB (6.36%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    
+    // int numDecodings(int pos,string s,vector<int>&mem){
+    //     int n=s.size();
+    //     if(mem[pos]>-1)return mem[pos];
+    //     // if(pos==s.size())return 1;
+    //     if(s[pos]=='0')return mem[pos]= 0;
+    //     int res=numDecodings(pos+1,s,mem);
+    //     if(pos<n-1 && (s[pos]=='1' ||(s[pos]=='2' && s[pos+1]<'7')) )res+=numDecodings(pos+2,s,mem);
+    //     return mem[pos]= res;
+    // }
+    
+    
+    int dp[101]={[0 ... 100]=-1};
+    
+    int decoding(int idx,string s){
+        if(idx>=s.size())return 1;
+        if(dp[idx]!=-1)return dp[idx];
+        if(s[idx]=='0')return 0;
+        
+        int cnt=0;
+        if(s.size()-idx>=2){
+            if( s[idx]<'2' || (s[idx]=='2' && s[idx+1]<'7')){
+                cnt+=decoding(idx+2,s);
+            }
+        }
+        cnt+=decoding(idx+1,s);
+        return dp[idx]= cnt;
+    }
+    
+    
+    int numDecodings(string s) {
+       //  vector<int>mem(s.size()+1,-1);
+       //  mem[s.size()]=1;
+       // return s.size()==0?0:numDecodings(0,s,mem);
+        return decoding(0,s);
+    }
+};
