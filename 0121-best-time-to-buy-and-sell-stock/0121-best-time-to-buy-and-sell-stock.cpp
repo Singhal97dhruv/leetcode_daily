1395be54caf0b51338849dc167df2b017caa3f6a
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-17 23:04:51 +0530

    Time: 172 ms (5.99%), Space: 93.4 MB (28.44%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -10,13 +10,25 @@ public:
         // }
         // return res;
         
+        // int low=prices[0];
+        // int diff=0;
+        // for(int i=1;i<prices.size();i++){
+        //     if(prices[i]<low)low=prices[i];
+        //     else diff=max(diff,prices[i]-low);
+        // }
+        // return diff;
+        
         int low=prices[0];
-        int diff=0;
-        for(int i=1;i<prices.size();i++){
-            if(prices[i]<low)low=prices[i];
-            else diff=max(diff,prices[i]-low);
+        int res=0;
+        for(int i=0;i<prices.size();i++){
+            if(prices[i]<low){
+                low=prices[i];
+            }
+            else{
+                res=max(res,prices[i]-low);
+            }
         }
-        return diff;
+        return res;
         
     }
 };
