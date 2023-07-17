b92942f16fff8d984f8f27d57a67a1614d4a41e5
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-17 22:59:19 +0530

    Time: 8 ms (71.19%), Space: 14.7 MB (55.15%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -69,18 +69,47 @@ public:
 //         return ans->next;
         
         
+//         if(list1==NULL)return list2;
+//         if(list2==NULL)return list1;
+        
+//         if(list1->val<list2->val){
+//             list1->next=mergeTwoLists(list1->next,list2);
+//             return list1;
+//         }
+        
+//         list2->next=mergeTwoLists(list1,list2->next);
+//         return list2;
+        
         if(list1==NULL)return list2;
         if(list2==NULL)return list1;
         
-        if(list1->val<list2->val){
-            list1->next=mergeTwoLists(list1->next,list2);
-            return list1;
+        ListNode*res=new ListNode(0);
+        ListNode*ans=res;
+        while(list1 && list2){
+            if(list1->val<list2->val){
+                res->next=list1;
+                list1=list1->next;
+            }
+            else{
+                res->next=list2;
+                list2=list2->next;
+            }
+            res=res->next;
         }
         
-        list2->next=mergeTwoLists(list1,list2->next);
-        return list2;
+        while(list1){
+            res->next=list1;
+            list1=list1->next;
+            res=res->next;
+        }
         
+        while(list2){
+            res->next=list2;
+            list2=list2->next;
+            res=res->next;
+        }
         
+        return ans->next;
         
     }
 };
