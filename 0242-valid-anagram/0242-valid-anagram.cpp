d4795a9b394ed157775ba9e12f6e05d5c268699a
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-17 23:34:00 +0530

    Time: 11 ms (62.18%), Space: 7.3 MB (79.97%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -14,17 +14,27 @@ public:
        //      count--;
        //  }        
        // return count==0?true:false;
+//         if(s.size()!=t.size())return false;
+        
+//         unordered_map<char,int>map;
+//         for (char c:s)map[c]++;
+        
+//         for(char c: t){
+//             if(!map[c])return false;
+//             map[c]--;
+//             if(map[c]==0)map.erase(c);
+//         }
+//         return map.size()==0?true:false;
         if(s.size()!=t.size())return false;
-        
         unordered_map<char,int>map;
-        for (char c:s)map[c]++;
-        
-        for(char c: t){
-            if(!map[c])return false;
-            map[c]--;
+        for(char c:s)map[c]++;
+        for(char c:t){
+            if(map[c]){
+                map[c]--;
+            }
+            else return false;
             if(map[c]==0)map.erase(c);
         }
-        return map.size()==0?true:false;
-        
+        return map.size()==0;
     }
 };
