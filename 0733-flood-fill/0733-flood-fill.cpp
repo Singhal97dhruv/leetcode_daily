db6e7e9aa7cd9d20591fbb8bf698b434af597bf2
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-17 23:45:32 +0530

    Time: 11 ms (57.98%), Space: 14.1 MB (50.78%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -12,24 +12,35 @@ public:
 
 //     }
     
-    vector<int>offset{0,1,0,-1,0};
-    void dfs(vector<vector<int>>&img,int i,int j,int color,int colToSet){
-        if(i<0 || j<0 || i>=img.size() || j>=img[0].size() || img[i][j]==colToSet || img[i][j]!=color)return ;
+//     vector<int>offset{0,1,0,-1,0};
+//     void dfs(vector<vector<int>>&img,int i,int j,int color,int colToSet){
+//         if(i<0 || j<0 || i>=img.size() || j>=img[0].size() || img[i][j]==colToSet || img[i][j]!=color)return ;
         
-        img[i][j]=colToSet;
-        for(int k=0;k<4;k++){
-            dfs(img,i+offset[k],j+offset[k+1],color,colToSet);
-        }
+//         img[i][j]=colToSet;
+//         for(int k=0;k<4;k++){
+//             dfs(img,i+offset[k],j+offset[k+1],color,colToSet);
+//         }
         
         
-    }
+//     }
     
+    
+    vector<int>offset{0,1,0,-1,0};
+    void fillMat(int i,int j,vector<vector<int>>&img,int col,int toBeChange){
+        if(i<0 || j<0 || i>=img.size() || j>=img[0].size() || img[i][j]==col || img[i][j]!=toBeChange)return;
+        img[i][j]=col;
+        for(int k=0;k<4;k++){
+            fillMat(i+offset[k],j+offset[k+1],img,col,toBeChange);
+        }
+    }
     
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         // dfs(image,sr,sc,color,image[sr][sc]);
         // return image;
         
-        dfs(image,sr,sc,image[sr][sc],color);
+        // dfs(image,sr,sc,image[sr][sc],color);
+        
+        fillMat(sr,sc,image,color,image[sr][sc]);
         
         return image;
     }
