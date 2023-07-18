c97209114e280ade558288563bc0dd62ddf6753e
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-19 00:14:31 +0530

    Time: 4 ms (80.37%), Space: 13.7 MB (12.60%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -56,10 +56,25 @@ public:
 //             res.push_back(temp);
 //         }
 //         return res;
+        // if(!root)return {};
+        // traverse (root,0);
+        // return res;
         if(!root)return {};
-        traverse (root,0);
+        queue<TreeNode*>q;
+        vector<vector<int>>res;
+        q.push(root);
+        while(!q.empty()){
+            int n=q.size();
+            vector<int>temp;
+            for(int i=0;i<n;i++){
+                TreeNode*t=q.front();q.pop();
+                temp.push_back(t->val);
+                if(t->left)q.push(t->left);
+                if(t->right)q.push(t->right);
+            }
+            res.push_back(temp);
+        }
         return res;
-        
         
         
     }
