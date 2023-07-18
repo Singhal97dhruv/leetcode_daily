07d7d136baa6af68956e56489afeaf9f5aa29e35
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-19 00:45:17 +0530

    Time: 7 ms (40.82%), Space: 8.6 MB (95.46%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -45,23 +45,40 @@ if(!node){
 //         }
 //         return copy;
         
-        Node*dup=new Node(node->val);
+//         Node*dup=new Node(node->val);
         
-        copies[node]=dup;
+//         copies[node]=dup;
+//         queue<Node*>q;
+//         q.push(node);
+//         while(!q.empty()){
+//             Node*temp=q.front();q.pop();
+//             for(Node*v:temp->neighbors){
+//                 if(copies.find(v)==copies.end()){
+//                     copies[v]=new Node(v->val,{});
+//                     q.push(v);
+//                 }
+//                 copies[temp]->neighbors.push_back(copies[v]);
+//             }
+//         }
+        
+//         return dup;
+        
+        Node*t=new Node(node->val);
+        
+        copies[node]=t;
         queue<Node*>q;
         q.push(node);
         while(!q.empty()){
             Node*temp=q.front();q.pop();
             for(Node*v:temp->neighbors){
                 if(copies.find(v)==copies.end()){
-                    copies[v]=new Node(v->val,{});
+                    copies[v]=new Node(v->val);
                     q.push(v);
                 }
                 copies[temp]->neighbors.push_back(copies[v]);
             }
         }
-        
-        return dup;
+        return t;
     }
 private:
     unordered_map<Node*,Node*>copies;
