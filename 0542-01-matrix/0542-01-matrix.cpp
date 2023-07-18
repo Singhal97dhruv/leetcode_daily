5da71088fbd8c8c72ad842b17660a9b3695b17eb
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-18 22:40:15 +0530

    Time: 83 ms (91.18%), Space: 30 MB (90.49%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -26,33 +26,26 @@ public:
 //         }
 //         return mat;
         
-        
+       
         vector<int>offset{0,1,0,-1,0};
         queue<pair<int,int>>q;
         for(int i=0;i<mat.size();i++){
             for(int j=0;j<mat[0].size();j++){
                 if(mat[i][j]==0)q.push({i,j});
-                else{
-                    mat[i][j]=-1;
-                }
+                else mat[i][j]=-1;
             }
         }
         
         while(!q.empty()){
-            auto[i,j]=q.front();q.pop();
-            
+            auto [i,j]=q.front();q.pop();
             for(int k=0;k<4;k++){
                 int nr=i+offset[k],nc=j+offset[k+1];
-                if(nr<0 || nc<0 || nr>=mat.size() || nc>=mat[0].size()|| mat[nr][nc]!=-1)continue;
+                if(nr<0 || nc<0 || nr>=mat.size() || nc>=mat[0].size() || mat[nr][nc]!=-1)continue;
                 mat[nr][nc]=mat[i][j]+1;
                 q.push({nr,nc});
             }
-            
-            
         }
-        
         return mat;
-        
         
     }
 };
