a79bf1bfb0d5d3460ebc625a3421d57131701866
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-18 23:07:42 +0530

    Time: 11 ms (74.21%), Space: 8 MB (66.07%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -18,13 +18,12 @@ public:
         // }
         // return false;
         
-        ListNode*fast=head,*slow=head;
+      ListNode*fast=head,*slow=head;
         while(fast && fast->next){
             fast=fast->next->next;
             slow=slow->next;
             if(fast==slow)return true;
         }
         return false;
-        
     }
 };
