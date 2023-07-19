cd120c7e3c74b134c0e7ea4a920d179e505a5bbe
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-20 01:48:13 +0530

    Time: 3 ms (79.41%), Space: 11 MB (91.88%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -49,27 +49,48 @@ public:
         // return nums[l]==target?l:-1;
         
         
-        int left=0,right=nums.size()-1;
+//         int left=0,right=nums.size()-1;
         
-        while(left<right){
+//         while(left<right){
             
-            int mid=left+(right-left)/2;
+//             int mid=left+(right-left)/2;
             
-            if(nums[mid]==target)return mid;
+//             if(nums[mid]==target)return mid;
             
-            if(nums[left]>nums[mid]){
+//             if(nums[left]>nums[mid]){
                 
+//                 if(target<nums[left] && target>nums[mid])left=mid+1;
+//                 else right=mid;
+                
+//             }
+
+//             else if(nums[right]<nums[mid]){
+//                 if(target<nums[mid] && target>=nums[left])right=mid;
+//                 else left=mid+1;
+//             }
+            
+//             else {
+//                 if(target<nums[mid])right=mid;
+//                 else left=mid+1;
+//             }
+//         }
+        
+//         return nums[left]==target?left:-1;
+        
+        
+        int left=0,right=nums.size()-1;
+        while(left<right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target)return mid;
+            if(nums[left]>nums[mid]){
                 if(target<nums[left] && target>nums[mid])left=mid+1;
                 else right=mid;
-                
             }
-
-            else if(nums[right]<nums[mid]){
-                if(target<nums[mid] && target>=nums[left])right=mid;
+            else if(nums[mid]>nums[right]){
+                if(target<nums[mid] && target>nums[right])right=mid;
                 else left=mid+1;
             }
-            
-            else {
+            else{
                 if(target<nums[mid])right=mid;
                 else left=mid+1;
             }
