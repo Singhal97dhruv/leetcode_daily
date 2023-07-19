8efdebd7836f360de230141786982c487dca4029
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-20 00:49:59 +0530

    Time: 44 ms (50.37%), Space: 12.3 MB (76.92%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -10,6 +10,15 @@ public:
         
 //     }
     
+    void dfs(int i,int j,vector<vector<char>>&grid){
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j]=='0')return;
+        grid[i][j]='0';
+
+        for(int k=0;k<4;k++){
+            dfs(i+offset[k],j+offset[k+1],grid);
+        }
+    }
+    
     int numIslands(vector<vector<char>>& grid) {
         
         // int cnt=0;
@@ -23,39 +32,48 @@ public:
         // }
         // return cnt;
         
-        int cnt=0;
+//         int cnt=0;
         
-        for(int i=0;i<grid.size();i++){
+//         for(int i=0;i<grid.size();i++){
+//             for(int j=0;j<grid[0].size();j++){
+//                 if(grid[i][j]=='1'){
+//                     cnt++;
+                    
+//                     queue<pair<int,int>>q;
+//                     q.push({i,j});
+                    
+//                     while(!q.empty()){
+//                         auto [s,t]=q.front();q.pop();
+                        
+//                         grid[s][t]='0';
+                        
+//                         for(int k=0;k<4;k++){
+//                             int r=s+offset[k],c=t+offset[k+1];
+//                             if(r<0 || c<0 || r>=grid.size() || c>=grid[0].size() || grid[r][c]=='0')continue;
+//                             grid[r][c]='0';
+//                             q.push({r,c});
+//                         }
+                        
+//                     }
+                    
+//                 }
+//             }
+//         }
+        
+//       return cnt;  
+        
+        int cnt=0;
+        for(int i=0;i<grid.size();i++)
+        {
             for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j]=='1'){
                     cnt++;
-                    
-                    queue<pair<int,int>>q;
-                    q.push({i,j});
-                    
-                    while(!q.empty()){
-                        auto [s,t]=q.front();q.pop();
-                        
-                        grid[s][t]='0';
-                        
-                        for(int k=0;k<4;k++){
-                            int r=s+offset[k],c=t+offset[k+1];
-                            if(r<0 || c<0 || r>=grid.size() || c>=grid[0].size() || grid[r][c]=='0')continue;
-                            grid[r][c]='0';
-                            q.push({r,c});
-                        }
-                        
-                    }
-                    
+                    dfs(i,j,grid);
                 }
             }
         }
         
-      return cnt;  
         
-        
-        
-        
-        
+        return cnt;
     }
 };
