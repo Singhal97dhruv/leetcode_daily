8e7fd8e8aff457ba44e682bd2f4380f82ef4e5bb
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-20 02:28:15 +0530

    Time: 4 ms (48.44%), Space: 7.9 MB (30.26%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -3,22 +3,36 @@ public:
     
     vector<vector<int>>res;
     
+//     void permute(int st,vector<int>&nums){
+//         if(st==nums.size()){
+//             res.push_back(nums);
+//             return;
+//         }
+        
+//         for(int i=st;i<nums.size();i++){
+            
+//             swap(nums[i],nums[st]);
+//             permute(st+1,nums);
+            
+//             swap(nums[i],nums[st]);
+//         }
+        
+    
+//     }
+    
     void permute(int st,vector<int>&nums){
         if(st==nums.size()){
-            res.push_back(nums);
-            return;
+            res.push_back(nums);return;
         }
         
         for(int i=st;i<nums.size();i++){
-            
             swap(nums[i],nums[st]);
             permute(st+1,nums);
-            
             swap(nums[i],nums[st]);
         }
         
-    
     }
+    
     
     vector<vector<int>> permute(vector<int>& nums) {
         
