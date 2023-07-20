f03583b3c9b9f1d7b5f93155eb7000a17873f6c5
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-21 02:25:34 +0530

    Time: 21 ms (22.28%), Space: 17.9 MB (24.07%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -2,29 +2,52 @@ class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         
-        stack<int>st;
+//         stack<int>st;
         
-        for(int i=0;i<asteroids.size();i++){
-            if(asteroids[i]>0)st.push(asteroids[i]);
-            else{
-                while(!st.empty() && st.top()>0 && st.top()<abs(asteroids[i])){
-                    st.pop();
-                }
-                if(!st.empty() && st.top()>0){
-                    if(st.top()==abs(asteroids[i])){st.pop();}
+//         for(int i=0;i<asteroids.size();i++){
+//             if(asteroids[i]>0)st.push(asteroids[i]);
+//             else{
+//                 while(!st.empty() && st.top()>0 && st.top()<abs(asteroids[i])){
+//                     st.pop();
+//                 }
+//                 if(!st.empty() && st.top()>0){
+//                     if(st.top()==abs(asteroids[i])){st.pop();}
                     
+//                 }
+//                 else{
+//                     st.push(asteroids[i]);
+//                 }
+//             }
+//         }
+        
+//         vector<int>res(st.size());
+//         for(int i=st.size()-1;i>=0;i--){
+//             res[i]=st.top();st.pop();
+//         }
+
+//         return res;
+        
+        
+        stack<int>stk;
+        for(int i:asteroids){
+            if(i>0)stk.push(i);
+            else{
+                while(!stk.empty() && stk.top()>0 && stk.top()<-i){
+                    stk.pop();
                 }
-                else{
-                    st.push(asteroids[i]);
+                if(!stk.empty() && stk.top()>0)
+                {
+                    if(stk.top()==-i)
+                    stk.pop();
                 }
+                else stk.push(i);
             }
         }
         
-        vector<int>res(st.size());
-        for(int i=st.size()-1;i>=0;i--){
-            res[i]=st.top();st.pop();
+        vector<int>res(stk.size());
+        for(int i=stk.size()-1;i>=0;i--){
+            res[i]=stk.top();stk.pop();
         }
-
         return res;
         
         
