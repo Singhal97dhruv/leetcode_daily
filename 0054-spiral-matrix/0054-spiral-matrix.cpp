03d9dea4b397222488fe6e46d12baa5739eac07a
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-22 00:22:21 +0530

    Time: 4 ms (34.12%), Space: 7 MB (21.99%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -32,46 +32,41 @@ public:
 //         }
 //         return res;
         
+        
         vector<int>res;
-        int m=matrix.size()-1,n=matrix[0].size()-1;
-        int left=0,top=0,right=m,bottom=n;
+        int top=0,left=0,right=matrix[0].size()-1,bottom=matrix.size()-1;
         
         int dirn=0;
         
         while(left<=right && top<=bottom){
             
             if(dirn==0){
-                for(int j=top;j<=bottom;j++){
-                    res.push_back(matrix[left][j]);
-                }
-                left++;
-            }
-            
-            else if(dirn==1){
                 for(int i=left;i<=right;i++){
-                    res.push_back(matrix[i][bottom]);
+                    res.push_back(matrix[top][i]);
                 }
-                bottom--;
+            top++;
             }
-            
-            else if(dirn==2){
-                for(int j=bottom;j>=top;j--){
-                    res.push_back(matrix[right][j]);
+            else if(dirn==1){
+                for(int i=top;i<=bottom;i++){
+                    res.push_back(matrix[i][right]);
                 }
                 right--;
             }
-            
-            else if(dirn==3){
+            else if(dirn==2){
                 for(int i=right;i>=left;i--){
-                    res.push_back(matrix[i][top]);
+                    res.push_back(matrix[bottom][i]);
                 }
-                top++;
+                bottom--;
             }
-            
+            else {
+                for(int i=bottom;i>=top;i--){
+                    res.push_back(matrix[i][left]);
+                }
+                left++;
+            }
             dirn=(dirn+1)%4;
             
         }
-        
         return res;
         
         
