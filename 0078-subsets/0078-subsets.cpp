b16eb545a95cd6042aa2286e1e172c0415bba9d7
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-22 00:35:00 +0530

    Time: 5 ms (36.84%), Space: 7.1 MB (60.46%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -3,13 +3,30 @@ public:
     
     vector<vector<int>>res;
     
-    void recur(vector<int>&temp,vector<int>&nums,int st){
-        if(st==nums.size())return;
+//     void recur(vector<int>&temp,vector<int>&nums,int st){
+//         if(st==nums.size())return;
         
-        for(int i=st;i<nums.size();i++){
+//         for(int i=st;i<nums.size();i++){
+//             temp.push_back(nums[i]);
+//             res.push_back(temp);
+//             recur(temp,nums,i+1);
+//             temp.pop_back();
+//         }
+        
+//     }
+    
+    
+    void recur(int idx,vector<int>&nums,vector<int>&temp){
+        if(idx>=nums.size()){
+            // res.push_back(temp);
+            return;
+        }
+        // recur(idx+1,nums,temp);
+        
+        for(int i=idx;i<nums.size();i++){
             temp.push_back(nums[i]);
             res.push_back(temp);
-            recur(temp,nums,i+1);
+            recur(i+1,nums,temp);
             temp.pop_back();
         }
         
@@ -18,7 +35,8 @@ public:
     vector<vector<int>> subsets(vector<int>& nums) {
         res.push_back({});
         vector<int>temp;
-        recur(temp,nums,0);
+        // recur(temp,nums,0);
+        recur(0,nums,temp);
         return res;
     }
 };
