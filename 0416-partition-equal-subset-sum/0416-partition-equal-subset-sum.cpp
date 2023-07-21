b10b0e310c86e0b812e5487c307fd9ab6467f301
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-21 23:58:19 +0530

    Time: 89 ms (95.42%), Space: 10.8 MB (87.65%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -26,13 +26,27 @@ public:
 //         }
 //         return dp[sum];
      
-        int sum =accumulate(nums.begin(),nums.end(),0);
+//         int sum =accumulate(nums.begin(),nums.end(),0);
+//         int target=sum/2;
+//         if(sum&1)return false;
+//         bitset<10001>dp(1);
+//         for(int i:nums){
+//             dp|=dp<<i;
+//         }
+//         return dp[target];
+        
+        int sum=accumulate(nums.begin(),nums.end(),0);
         int target=sum/2;
+        vector<int>dp(target+1,0);
+        dp[0]=1;
         if(sum&1)return false;
-        bitset<10001>dp(1);
         for(int i:nums){
-            dp|=dp<<i;
+            for(int j=target;j>=i;j--){
+                if(dp[j-i])
+                    dp[j]=1;
+            }
         }
         return dp[target];
+        
     }
 };
