c5178a8983a09c67d3b45cadf45e64b522b10525
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-23 00:56:34 +0530

    Time: 19 ms (96.10%), Space: 13.5 MB (38.32%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -24,21 +24,18 @@ public:
 //     }
     
     
-    ListNode*mergeTwoSortedLists(ListNode*l1,ListNode*l2){
-        if(!l1 && !l2)return NULL;
-        if(!l1)return l2;
-        if(!l2)return l1;
-        
-        if(l1->val<l2->val){
-            l1->next=mergeTwoSortedLists(l1->next,l2);
-            return l1;
-        }
-                
-            l2->next=mergeTwoSortedLists(l1,l2->next);
-            return l2;
-        
-        
-    }
+  ListNode*mergeTwoSortedLists(ListNode*l1,ListNode*l2){
+      if(!l1)return l2;
+      if(!l2)return l1;
+      
+      if(l1->val<l2->val){
+          l1->next=mergeTwoSortedLists(l1->next,l2);
+        return l1;    
+      }
+      l2->next=mergeTwoSortedLists(l1,l2->next);
+      return l2;
+      
+  }
     
     
     
@@ -81,15 +78,26 @@ public:
         // return lists.front();
         
         
-        int n =lists.size();
-        if(n==0)return NULL;
+//         int n =lists.size();
+//         if(n==0)return NULL;
+//         while(n>1){
+//             for(int i=0;i<n/2;i++){
+//                 lists[i]=mergeTwoSortedLists(lists[i],lists[n-i-1]);
+//             }
+//             n=(n+1)/2;
+//         }
+        
+//         return lists.front();
+        
+        int n=lists.size();
+        if (n==0)return NULL;
         while(n>1){
             for(int i=0;i<n/2;i++){
                 lists[i]=mergeTwoSortedLists(lists[i],lists[n-i-1]);
+                
             }
             n=(n+1)/2;
         }
-        
         return lists.front();
     }
 };
