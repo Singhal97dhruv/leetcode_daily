437af1c7c3ff82653cb782f3c04431f55b0e1afa
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-24 00:02:39 +0530

    Time: 16 ms (82.90%), Space: 26.5 MB (20.23%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -14,27 +14,25 @@ public:
     
     unordered_map<int,int>map;
     
-    TreeNode*build(vector<int>&pre,vector<int>&in,int &rootIdx,int left ,int right){
+    TreeNode*build(vector<int>&pre,vector<int>&in,int &RootIdx,int left,int right){
         
         if(left>right)return NULL;
-        int p=map[pre[rootIdx]];
-        rootIdx++;
-        TreeNode*t=new TreeNode(in[p]);
-        t->left=build(pre,in,rootIdx,left,p-1);
-        t->right=build(pre,in,rootIdx,p+1,right);
-        return t;
         
-    }
-    
+        int idx=map[pre[RootIdx++]];
+        TreeNode*t=new TreeNode(in[idx]);
+        
+        t->left=build(pre,in,RootIdx,left,idx-1);
+        t->right=build(pre,in,RootIdx,idx+1,right);
+        
+        return t;
+    }    
     
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         
         for(int i=0;i<inorder.size();i++){
             map[inorder[i]]=i;
         }
-        int rootIdx=0;        
-        return build(preorder,inorder,rootIdx,0,inorder.size()-1);
-        
-        
+        int RootIdx=0;
+        return build(preorder,inorder,RootIdx,0,preorder.size()-1);
     }
 };
