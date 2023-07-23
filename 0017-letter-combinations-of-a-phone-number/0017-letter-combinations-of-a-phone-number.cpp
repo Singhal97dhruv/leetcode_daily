271382c20d31423fb944b35a961a99bc390242b4
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-24 00:19:49 +0530

    Time: 5 ms (9.39%), Space: 6.5 MB (55.83%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -4,25 +4,39 @@ public:
     
     vector<string>offset={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     
+//     vector<string>res;
+    
+//     void recur(string s,int st,string dig){
+//         if(st==dig.size()){
+//             res.push_back(s);
+//             return;
+//         }
+//         int idx=dig[st]-'0';
+//         int n=offset[idx].size();
+        
+//         for(int i=0;i<n;i++){
+//             s+=offset[idx][i];
+//             recur(s,st+1,dig);
+//             s.pop_back();
+//         }
+        
+        
+//     }
+    
     vector<string>res;
     
-    void recur(string s,int st,string dig){
-        if(st==dig.size()){
-            res.push_back(s);
-            return;
+    void backtrack(int idx,string curr,string digits){
+        if(idx>=digits.size()){
+            res.push_back(curr);
+            return ;
         }
-        int idx=dig[st]-'0';
-        int n=offset[idx].size();
+        int offsetIdx=digits[idx]-'0';
         
-        for(int i=0;i<n;i++){
-            s+=offset[idx][i];
-            recur(s,st+1,dig);
-            s.pop_back();
+        for(int i=0;i<offset[offsetIdx].size();i++){
+            backtrack(idx+1,curr+offset[offsetIdx][i],digits);
         }
-        
         
     }
-    
     
     
     vector<string> letterCombinations(string digits) {
@@ -40,7 +54,9 @@ public:
 //         return result;
         
         if(digits.size()==0)return {};
-        recur("",0,digits);
+        // recur("",0,digits);
+        
+        backtrack(0,"",digits);
         return res;
         
     }
