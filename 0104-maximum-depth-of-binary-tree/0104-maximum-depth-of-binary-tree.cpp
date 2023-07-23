a0b2275b8fc121056c32a440ca40e69fb48b419b
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-23 23:30:20 +0530

    Time: 4 ms (95.20%), Space: 18.8 MB (28.31%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -11,6 +11,16 @@
  */
 class Solution {
 public:
+    
+    int lev=1;
+
+    void depth(TreeNode*t,int d){
+        if(!t)return ;
+        lev=max(lev,d);
+        depth(t->left,d+1);
+        depth(t->right,d+1);
+    }
+    
     int maxDepth(TreeNode* root) {
         if(root==NULL)return 0;
     //     int lval=maxDepth(root->left);
@@ -21,25 +31,44 @@ public:
     //     else{
     //         return rval+1;
     //     }
-    queue<TreeNode*>q;
-        q.push(root);
-        int res=0;
-        while(!q.empty()){
-            res++;
-            int n;
-            for(int i=0,n=q.size();i<n;i++){
-                TreeNode *curr=q.front();
-                q.pop();
-                if(curr->left!=NULL){
-                    q.push(curr->left);
-                }
-                if(curr->right!=NULL){
-                    q.push(curr->right);
-                }
-            }
-        }
-    return res;
+    // queue<TreeNode*>q;
+    //     q.push(root);
+    //     int res=0;
+    //     while(!q.empty()){
+    //         res++;
+    //         int n;
+    //         for(int i=0,n=q.size();i<n;i++){
+    //             TreeNode *curr=q.front();
+    //             q.pop();
+    //             if(curr->left!=NULL){
+    //                 q.push(curr->left);
+    //             }
+    //             if(curr->right!=NULL){
+    //                 q.push(curr->right);
+    //             }
+    //         }
+    //     }
+    // return res;
         
+//         int cnt=0;
+//         queue<TreeNode*>q;
+//         q.push(root);
+//         while(!q.empty())
+//         {
+//             int n=q.size();
+//             cnt++;
+//             for(int i=0;i<n;i++){
+//                 TreeNode*t=q.front();q.pop();
+//                 if(t->left)q.push(t->left);
+//                 if(t->right)q.push(t->right);
+//             }
+            
+            
+//         }
+//         return cnt;
+        if(!root)return 0;
+        depth(root,1);
+        return lev;
         
     }
 };
