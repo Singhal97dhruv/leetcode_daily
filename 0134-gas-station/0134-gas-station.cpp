9a7d85bde0daf52525c1310c4e4e27a6d66e515c
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-25 02:44:19 +0530

    Time: 165 ms (84.22%), Space: 128.2 MB (46.30%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-       int n = gas.size();
-        int total_surplus = 0;
-        int surplus = 0;
-        int start = 0;
+//        int n = gas.size();
+//         int total_surplus = 0;
+//         int surplus = 0;
+//         int start = 0;
         
-        for(int i = 0; i < n; i++){
-            total_surplus += gas[i] - cost[i];
-            surplus += gas[i] - cost[i];
-            if(surplus < 0){
-                surplus = 0;
-                start = i + 1;
+//         for(int i = 0; i < n; i++){
+//             total_surplus += gas[i] - cost[i];
+//             surplus += gas[i] - cost[i];
+//             if(surplus < 0){
+//                 surplus = 0;
+//                 start = i + 1;
+//             }
+//         }
+//         return (total_surplus < 0) ? -1 : start;
+        
+        
+        int n=gas.size();
+        int totalSurplus=0,surPlus=0,start=0;
+        for(int i=0;i<n;i++){
+            totalSurplus+=gas[i]-cost[i];
+            surPlus+=gas[i]-cost[i];
+            if(surPlus<0){
+                surPlus=0;
+                start=i+1;
             }
         }
-        return (total_surplus < 0) ? -1 : start;
-        
+        return totalSurplus>=0?start:-1;
     }
 };
