b97188618db042ec626e23e7b3706f2b17ef183e
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-25 00:15:17 +0530

    Time: 3 ms (38.05%), Space: 7.5 MB (99.91%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
+//     int dp[101]={[0 ... 100]=-1};
+//     int recur(vector<int>&nums,int idx){
+        
+//         if(idx>=nums.size())return 0;
+        
+//         if(dp[idx]!=-1)return dp[idx];
+        
+//         return dp[idx]= max(nums[idx]+recur(nums,idx+2),recur(nums,idx+1));
+        
+//     }
     int dp[101]={[0 ... 100]=-1};
-    int recur(vector<int>&nums,int idx){
-        
+    int recur(int idx,vector<int>&nums){
         if(idx>=nums.size())return 0;
-        
         if(dp[idx]!=-1)return dp[idx];
+        int ans=0;
+        ans=nums[idx]+recur(idx+2,nums);
         
-        return dp[idx]= max(nums[idx]+recur(nums,idx+2),recur(nums,idx+1));
-        
+        ans=max(nums[idx]+recur(idx+2,nums),recur(idx+1,nums));
+        return dp[idx]=ans;
     }
     
     
@@ -24,12 +34,15 @@ public:
         
         // return recur(nums,0);
         
-        vector<int>dp(nums.size()+2,0);
+//         vector<int>dp(nums.size()+2,0);
         
         
-        for(int i=nums.size()-1;i>=0;i--){
-            dp[i]=max(dp[i+1],nums[i]+dp[i+2]);
-        }
-        return dp[0];
+//         for(int i=nums.size()-1;i>=0;i--){
+//             dp[i]=max(dp[i+1],nums[i]+dp[i+2]);
+//         }
+//         return dp[0];
+        return recur(0,nums);
+        
+        
     }
 };
