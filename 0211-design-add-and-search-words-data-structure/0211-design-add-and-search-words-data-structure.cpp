1b3be1359cd90b5019f83f81910e12e243c0d8f9
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-26 01:20:21 +0530

    Time: 1083 ms (39.27%), Space: 628.6 MB (11.32%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
@@ -1,43 +1,69 @@
+   class Trie{
+       public:
+        vector<Trie*>children;
+        bool isEnd;
+        Trie(){
+            children= vector<Trie*>(26,NULL);
+            isEnd=false;
+        }
+    };
 class WordDictionary {
-    
-    vector<WordDictionary*>children;
-    bool isEnd;
-    
 public:
+    
+    Trie*root= new Trie();
+    
     WordDictionary() {
-        isEnd=false;
-        children=vector<WordDictionary*>(26,NULL);
+        
     }
     
     void addWord(string word) {
-        WordDictionary *t=this;
+        Trie*t=root;
         for(char c:word){
-            if(t->children[c-'a']==NULL)t->children[c-'a']=new WordDictionary();
-            t=t->children[c-'a'];
+            if(t->children[c-'a'])t=t->children[c-'a'];
+            else {
+                t->children[c-'a']=new Trie();
+                t=t->children[c-'a'];
+            }
         }
         t->isEnd=true;
     }
     
     bool search(string word) {
-        WordDictionary *t=this;
+//         Trie*t=root;
+//         for(int i=0;i<word.size();i++){
+//             if(word[i]=='.'){
+//                 for(int j=0;j<26;j++){
+//                     if(t->children[j] && search(word.substr(i+1)))return true;
+//                 }
+//             }
+//             else if(t->children[word[i]-'a']){
+//                 t=t->children[word[i]-'a'];
+//             }
+//         }
         
-        for(int i=0;i<word.size();i++){
-            
-            if(word[i]=='.'){
-                for(auto ch:t->children){
-                    if(ch && ch->search(word.substr(i+1)))return true;}
-                    return false;
-            }
-            else{
-                if(t->children[word[i]-'a']==NULL)return false;
-                t=t->children[word[i]-'a'];
-            }
-            
-        }
-        
-        return t && t->isEnd;
+//         return t && t->isEnd==true;
+        return searchHelper(root,word);
         
     }
+    
+    bool searchHelper(Trie*r,string word){
+        Trie*t=r;
+        for(int i=0;i<word.size();i++){
+            if(word[i]=='.'){
+                for(auto node: t->children){
+                    if(node && searchHelper(node,word.substr(i+1)))return true;
+                }
+                return false;
+            }
+            else if(t->children[word[i]-'a']){
+                t=t->children[word[i]-'a'];
+            }
+            else return false;
+        }
+        
+        return t && t->isEnd==true;
+    }
+    
 };
 
 /**
