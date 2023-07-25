ab9f94566fa826ebbac3afb0aa1b3420e06de1ac
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-25 23:49:28 +0530

    Time: 28 ms (44.97%), Space: 17.9 MB (92.12%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -18,34 +18,50 @@ public:
 //         return true;
         
         
+//         for(int i=0;i<9;i++){
+//             int arr[10]={0};
+//             for(int j=0;j<9;j++){
+//                 if(board[i][j]=='.')continue;
+//                 if(arr[board[i][j]-'0']==1)return false;
+//                 arr[board[i][j]-'0']=1;
+//             }
+//         }
+        
+//          for(int j=0;j<9;j++){
+//             int arr[10]={0};
+//             for(int i=0;i<9;i++){
+//                 if(board[i][j]=='.')continue;
+//                 if(arr[board[i][j]-'0']==1)return false;
+//                 arr[board[i][j]-'0']=1;
+//             }
+//         }
+        
+//         for(int i=0;i<9;i+=3){
+//             for(int j=0;j<9;j+=3){
+//                 int arr[10]={0};
+//                 for(int k=i;k<i+3;k++){
+//                     for(int l=j;l<j+3;l++){
+//                         if(board[k][l]=='.')continue;
+//                         if(arr[board[k][l]-'0']==1)return false;
+//                         arr[board[k][l]-'0']=1;
+//                     }
+//                 }
+//             }
+//         }
+        
+//         return true;
+        
+        int arr1[9][9]={0},arr2[9][9]={0},arr3[9][9]={0};
+        
         for(int i=0;i<9;i++){
-            int arr[10]={0};
             for(int j=0;j<9;j++){
                 if(board[i][j]=='.')continue;
-                if(arr[board[i][j]-'0']==1)return false;
-                arr[board[i][j]-'0']=1;
-            }
-        }
-        
-         for(int j=0;j<9;j++){
-            int arr[10]={0};
-            for(int i=0;i<9;i++){
-                if(board[i][j]=='.')continue;
-                if(arr[board[i][j]-'0']==1)return false;
-                arr[board[i][j]-'0']=1;
-            }
-        }
-        
-        for(int i=0;i<9;i+=3){
-            for(int j=0;j<9;j+=3){
-                int arr[10]={0};
-                for(int k=i;k<i+3;k++){
-                    for(int l=j;l<j+3;l++){
-                        if(board[k][l]=='.')continue;
-                        if(arr[board[k][l]-'0']==1)return false;
-                        arr[board[k][l]-'0']=1;
-                    }
-                }
+                int num=board[i][j]-'0'-1;
+                int blockIdx=(i/3)*3 + j/3; 
+                if(arr1[i][num] || arr2[j][num] || arr3[blockIdx][num])return false;
+                    
+                arr1[i][num]=arr2[j][num]=arr3[blockIdx][num]=1;                
+                
             }
         }
         
