8bebe2bc46b21aa72b73bc49ce9780ec4fe66037
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-25 23:55:39 +0530

    Time: 44 ms (46.88%), Space: 20.7 MB (37.28%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         
-        unordered_map<string,vector<string>>map;
+      unordered_map<string,vector<string>>map;
         for(string s:strs){
-            string t=s;
-            sort(t.begin(),t.end());
-            map[t].push_back(s);
+            string temp=s;
+            sort(s.begin(),s.end());
+                map[s].push_back(temp);
         }
-        vector<vector<string>>vect;
+        vector<vector<string>>res;
         for(auto mp:map){
-            vect.push_back(mp.second);
+            res.push_back(mp.second);
         }
-        return vect;
-        
+        return res;
         
 //         vector<int>primes={2, 3, 5, 7, 11 ,13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 107};
         
