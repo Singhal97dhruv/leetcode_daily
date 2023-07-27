5f82d38157e8bf3cca00d8a718d78e9af2ece3ae
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-27 23:57:52 +0530

    Time: 7 ms (99.98%), Space: 13.6 MB (69.62%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,31 +1,54 @@
 class Solution {
 public:
+    
+    bool isCycle(int u,vector<int>adjList[],vector<int>&vis,vector<int>&ans){
+        if(vis[u]==2)return false;
+        if(vis[u]==1)return true;
+        
+        vis[u]=1;
+        for(int v:adjList[u]){
+            if(isCycle(v,adjList,vis,ans))return true;
+        }
+        vis[u]=2;
+        ans.push_back(u);
+        return false;
+        
+    }
+    
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         
-        vector<int>adjList[numCourses],indegree(numCourses,0);
+        vector<int>adjList[numCourses];
         vector<int>ans;
         
         for(int i=0;i<prerequisites.size();i++){
             adjList[prerequisites[i][1]].push_back(prerequisites[i][0]);
-            indegree[prerequisites[i][0]]++;
+            // indegree[prerequisites[i][0]]++;
         }
+        
+        vector<int>vis(numCourses,0);
         for(int i=0;i<numCourses;i++){
-            int j=0;
-            for(;j<numCourses;j++){
-                if(!indegree[j]){
-                    ans.push_back(j);
-                    break;
-                }
-            }
-            
-            if(j==numCourses)return {};
-            
-            indegree[j]--;
-            for(auto v:adjList[j]){
-                indegree[v]--;
-            }
-            
+            if(isCycle(i,adjList,vis,ans))return {};
         }
+        reverse(ans.begin(),ans.end());
+        
+        
+//         for(int i=0;i<numCourses;i++){
+//             int j=0;
+//             for(;j<numCourses;j++){
+//                 if(!indegree[j]){
+//                     ans.push_back(j);
+//                     break;
+//                 }
+//             }
+            
+//             if(j==numCourses)return {};
+            
+//             indegree[j]--;
+//             for(auto v:adjList[j]){
+//                 indegree[v]--;
+//             }
+            
+//         }
         return ans;
         
         
