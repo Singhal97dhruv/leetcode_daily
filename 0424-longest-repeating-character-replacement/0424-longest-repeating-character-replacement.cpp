9b1688c8fe2528750995871f3025a05f1c784d3d
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-29 00:52:09 +0530

    Time: 3 ms (98.66%), Space: 6.9 MB (71.78%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        vector<int>vect(26,0);
-        int st=0;
-        int mxCharCnt=0;
-        int res=0;
-        for(int end=0;end<s.size();end++){
-            vect[s[end]-'A']++;
-            if(mxCharCnt<vect[s[end]-'A']){
-                mxCharCnt=vect[s[end]-'A'];
-            }
-            while(end-st-mxCharCnt+1>k){
-                vect[s[st]-'A']--;
-                st++;
-            }
-            res=max(res,end-st+1);
-        }
+//         vector<int>vect(26,0);
+//         int st=0;
+//         int mxCharCnt=0;
+//         int res=0;
+//         for(int end=0;end<s.size();end++){
+//             vect[s[end]-'A']++;
+//             if(mxCharCnt<vect[s[end]-'A']){
+//                 mxCharCnt=vect[s[end]-'A'];
+//             }
+//             while(end-st-mxCharCnt+1>k){
+//                 vect[s[st]-'A']--;
+//                 st++;
+//             }
+//             res=max(res,end-st+1);
+//         }
         
+//         return res;
+        
+        int left=0,res=0;
+        vector<int>alpha(26,0);
+        int mxCharCnt=0;
+        for(int right=0;right<s.size();right++){
+            alpha[s[right]-'A']++;
+            
+            if(mxCharCnt<alpha[s[right]-'A'])mxCharCnt=alpha[s[right]-'A'];
+            while(right-left-mxCharCnt+1>k){
+                alpha[s[left++]-'A']--;
+            }
+            res=max(res,right-left+1);
+            
+        }
         return res;
+        
     }
 };
