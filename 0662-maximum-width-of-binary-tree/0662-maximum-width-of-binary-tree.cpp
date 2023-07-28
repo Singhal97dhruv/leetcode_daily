58b1e37437c63dcde7775f893c8f3c549f3c1958
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-28 23:56:03 +0530

    Time: 4 ms (97.55%), Space: 17.2 MB (96.41%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -13,25 +13,47 @@ class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
         
+        // queue<pair<TreeNode*,int>>q;
+        // q.push({root,0});
+        // int res=0;
+        // while(q.size()){
+        //     int sz=q.size();
+        //     int firstNodeIdx=q.front().second;
+        //     int lastNodeIdx=q.back().second;
+        //     res=max(res,lastNodeIdx-firstNodeIdx+1);
+        //     for(int i=0;i<sz;i++){
+        //         pair<TreeNode*,int>pr=q.front();
+        //         q.pop();
+        //         int idx=pr.second-firstNodeIdx;
+        //         if(pr.first->left){
+        //             q.push({pr.first->left,(long long)2*idx+1});
+        //         }
+        //         if(pr.first->right){
+        //             q.push({pr.first->right,(long long)2*idx+2});
+        //         }
+        //     }
+        // }
+        // return res;
+        
         queue<pair<TreeNode*,int>>q;
         q.push({root,0});
         int res=0;
-        while(q.size()){
-            int sz=q.size();
-            int firstNodeIdx=q.front().second;
-            int lastNodeIdx=q.back().second;
-            res=max(res,lastNodeIdx-firstNodeIdx+1);
-            for(int i=0;i<sz;i++){
-                pair<TreeNode*,int>pr=q.front();
-                q.pop();
-                int idx=pr.second-firstNodeIdx;
-                if(pr.first->left){
-                    q.push({pr.first->left,(long long)2*idx+1});
+        while(!q.empty()){
+            int n=q.size();
+            int LeftNodeIdx=q.front().second;
+            int RightNodeIdx=q.back().second;
+            int temp=RightNodeIdx-LeftNodeIdx+1;
+            res=max(res,temp);
+            for(int i=0;i<n;i++){
+                auto [node,idx]=q.front();q.pop();
+                if(node->left){
+                    q.push({node->left,(long long)2*idx+1});
                 }
-                if(pr.first->right){
-                    q.push({pr.first->right,(long long)2*idx+2});
+                if(node->right){
+                    q.push({node->right,(long long)2*idx+2});
                 }
             }
+            
         }
         return res;
     }
