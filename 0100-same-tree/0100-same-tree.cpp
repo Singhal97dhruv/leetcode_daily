3098b692470dedf4cbdd0fcfc260dd6f13806535
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-29 01:45:18 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (5.60%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -13,14 +13,10 @@ class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         
-        if(!p && !q)return true;
-        if(!p || !q)return false;
-        return p->val==q->val && isSameTree(p->left,q->left)&& isSameTree(p->right,q->right);
+        // if(!p && !q)return true;
+        // if(!p || !q)return false;
+        // return p->val==q->val && isSameTree(p->left,q->left)&& isSameTree(p->right,q->right);
         
-//         if(!p && !q)return true;
-//         if(!p || !q)return false;
-        
-//         return (p->val==q->val && isSameTree(p->left,q->left)&& isSameTree(p->right,q->right));
     
 //         if(!p && !q)return true;
 //         if(!p || !q)return false;
@@ -50,6 +46,29 @@ public:
             
 //         }
 //         return q1.empty() && q2.empty();
+        
+        if(!p && !q)return true;
+        if(!p || !q)return false;
+        queue<TreeNode*>q1,q2;
+        q1.push(p);q2.push(q);
+        while(!q1.empty() && !q2.empty()){
+            int n=q1.size();
+            for(int i=0;i<n;i++){
+                TreeNode*t1=q1.front();q1.pop();
+                TreeNode*t2=q2.front();q2.pop();
+                if(t1->val!=t2->val)return false;
+                if(t1->left)q1.push(t1->left);
+                if(t2->left)q2.push(t2->left);
+                if(q1.size()!=q2.size())return false;
+                if(t1->right)q1.push(t1->right);
+                if(t2->right)q2.push(t2->right);
+                if(q1.size()!=q2.size())return false;
+                
+                }
+            }
+        
+        return q1.empty() && q2.empty();        
+        
     
     }
 };
