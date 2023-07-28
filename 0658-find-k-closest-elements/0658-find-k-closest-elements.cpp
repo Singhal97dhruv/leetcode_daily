d519a76e06f4e802fe43b4a4198d3f79858bf8e0
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-29 00:18:01 +0530

    Time: 83 ms (44.00%), Space: 34 MB (37.70%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,43 +1,57 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        int j=0;
-        vector<int>vect;
-        priority_queue<int,vector<int>,greater<int>>pq;
-        for(j;j<arr.size();j++){
-            if(arr[j]>x)break;
-        }
-        int i=j-1;
-        while(i<j && j<=arr.size()-1 && i>=0 && k>0){
-            int w=abs(arr[j]-x);
-            int y=abs(arr[i]-x);
-            int z=min(w,y);
-            if(w==y){pq.push(arr[i]);
-                    i--;}
-            else if(z==w){pq.push(arr[j]);
-                         j++;}
-            else {
-                pq.push(arr[i]);
-                i--;
-            }
-            k--;
+//         int j=0;
+//         vector<int>vect;
+//         priority_queue<int,vector<int>,greater<int>>pq;
+//         for(j;j<arr.size();j++){
+//             if(arr[j]>x)break;
+//         }
+//         int i=j-1;
+//         while(i<j && j<=arr.size()-1 && i>=0 && k>0){
+//             int w=abs(arr[j]-x);
+//             int y=abs(arr[i]-x);
+//             int z=min(w,y);
+//             if(w==y){pq.push(arr[i]);
+//                     i--;}
+//             else if(z==w){pq.push(arr[j]);
+//                          j++;}
+//             else {
+//                 pq.push(arr[i]);
+//                 i--;
+//             }
+//             k--;
             
+//         }
+//         while(i>=0 && k>0){
+//             pq.push(arr[i]);
+//             i--;
+//             k--;
+//         }
+//         while(j<=arr.size()-1 && k>0){
+//             pq.push(arr[j]);
+//             j++;
+//             k--;
+//         }
+//         int n=pq.size();
+//         for(int i=0;i<n;i++){
+//             vect.push_back(pq.top());
+//             pq.pop();
+//         }
+//         return vect;
+        
+        
+        vector<int>res;
+        priority_queue<pair<int,int>>pq;
+        for(int i: arr){
+            pq.push({abs(i-x),i});
+            if(pq.size()>k)pq.pop();
         }
-        while(i>=0 && k>0){
-            pq.push(arr[i]);
-            i--;
-            k--;
+        while(!pq.empty()){
+            res.push_back(pq.top().second);pq.pop();
         }
-        while(j<=arr.size()-1 && k>0){
-            pq.push(arr[j]);
-            j++;
-            k--;
-        }
-        int n=pq.size();
-        for(int i=0;i<n;i++){
-            vect.push_back(pq.top());
-            pq.pop();
-        }
-        return vect;
+        sort(res.begin(),res.end());
+        return res;
+        
     }
 };
