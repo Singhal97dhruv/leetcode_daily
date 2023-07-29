e2034c393ed23917ad18338f265bf9dac825c551
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-07-30 00:26:45 +0530

    Time: 42 ms (99.90%), Space: 48.4 MB (40.38%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,5 +1,16 @@
 class Solution {
 public:
+    
+//     bool reach(int idx,int jump,vector<int>&nums){
+//         if(idx+jump>=nums.size()-1)return true;
+//         bool ans=false;
+//         for(int i=idx+1;i<=idx+jump;i++){
+//             ans=ans|| reach(i,nums[i],nums);
+//         }
+//         return ans;
+//     }
+    
+    
     bool canJump(vector<int>& nums) {
 //         int n=0,i=0;
         
@@ -10,12 +21,19 @@ public:
 //         return false;
         
         
-        int jmpLen=0;
-        for(int i=0;i<=jmpLen;i++){
-            jmpLen=max(jmpLen,nums[i]+i);
-            if(jmpLen>=nums.size()-1)return true;
+       //  int jmpLen=0;
+       //  for(int i=0;i<=jmpLen;i++){
+       //      jmpLen=max(jmpLen,nums[i]+i);
+       //      if(jmpLen>=nums.size()-1)return true;
+       //  }
+       // return false;
+        // return reach(0,nums[0],nums);
+       
+        int mx=0;
+        for(int i=0;i<=mx;i++){
+            mx=max(mx,i+nums[i]);
+            if(mx>=nums.size()-1)return true;
         }
-       return false;
-        
+        return false;
     }
 };
