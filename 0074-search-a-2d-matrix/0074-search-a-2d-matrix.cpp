49c142301f2be54284410611cc8c2d987b3a7366
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-01 01:16:23 +0530

    Time: 3 ms (84.19%), Space: 9.5 MB (53.77%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -15,20 +15,30 @@ public:
         // }
         // return false;
         
-        int r=matrix.size(),c=matrix[0].size();
-        int left=0,right=r*c-1;
+//         int r=matrix.size(),c=matrix[0].size();
+//         int left=0,right=r*c-1;
 
+//         while(left<=right){
+//             int mid=left+(right-left)/2;
+//             int rowmid=mid/c,colmid=mid%c;
+                        
+//             if(matrix[rowmid][colmid]==target)return true;
+            
+//             else if(matrix[rowmid][colmid]<target)left=mid+1;
+//             else right=mid-1;
+            
+//         }
+        
+//         return false;
+        int m=matrix.size(),n=matrix[0].size();
+        int left=0,right=m*n-1;
         while(left<=right){
             int mid=left+(right-left)/2;
-            int rowmid=mid/c,colmid=mid%c;
-                        
-            if(matrix[rowmid][colmid]==target)return true;
-            
-            else if(matrix[rowmid][colmid]<target)left=mid+1;
-            else right=mid-1;
-            
+            int row=mid/n, col=mid%n;
+            if(target==matrix[row][col])return true;
+            else if(target<matrix[row][col])right=mid-1;
+            else left=mid+1;
         }
-        
         return false;
         
     }
