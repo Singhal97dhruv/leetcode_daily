f3896468fb68298fe166489d408b53bce63534ab
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-03 23:22:02 +0530

    Time: 8 ms (97.71%), Space: 13.5 MB (18.19%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,98 +1,67 @@
 class Solution {
 public:
     
-    void settingZeros(vector<vector<int>>&mat,int ii,int jj){
-        for(int i=0;i<mat.size();i++){
-            mat[i][jj]=0;
-        }
-        for(int j=0;j<mat[0].size();j++){
-            mat[ii][j]=0;
-        }
-    }
     
     void setZeroes(vector<vector<int>>& matrix) {
         
+        
+        
 //         bool row=false,col=false;
-//         for(int i=0;i<matrix.size();i++){
-//             if(matrix[i][0]==0){
-//                 row=true;
-//                 break;
-//             }
+//         int m=matrix.size(),n=matrix[0].size();
+        
+//         for(int i=0;i<m;i++){
+//             if(matrix[i][0]==0){col=true;break;}
 //         }
-//         for(int i=0;i<matrix[0].size();i++){
-//             if(matrix[0][i]==0){
-//                 col=true;
-//                 break;
-//             }
+//         for(int j=0;j<n;j++){
+//             if(matrix[0][j]==0){row=true;break;}
 //         }
-//         for(int i=1;i<matrix.size();i++){
-//             for(int j=1;j<matrix[0].size();j++){
+    
+//         for(int i=1;i<m;i++){
+//             for(int j=1;j<n;j++){
 //                 if(matrix[i][j]==0){
 //                     matrix[i][0]=0;
 //                     matrix[0][j]=0;
 //                 }
 //             }
 //         }
-//         for(int i=1;i<matrix.size();i++){
-//             for(int j=1;j<matrix[0].size();j++){
-//                     if(matrix[i][0]==0 || matrix[0][j]==0){
-//                         matrix[i][j]=0;
+//         for(int i=1;i<m;i++){
+//             for(int j=1;j<n;j++){
+//                 if(matrix[i][0]==0||matrix[0][j]==0){
+//                     matrix[i][j]=0;
 //                 }
 //             }
 //         }
-//         if(row)
-//         for(int i=0;i<matrix.size();i++){
-//             matrix[i][0]=0;
-            
-//         }
-//         if(col)
-//             for(int j=0;j<matrix[0].size();j++){
-//                 matrix[0][j]=0;
-//             }
-    
-//     vector<pair<int,int>>set;
-//         for(int i=0;i<matrix.size();i++){
-//             for(int j=0;j<matrix[0].size();j++){
-//                 if(matrix[i][j]==0)set.push_back({i,j});
-//             }
-//         }
         
-//         for(auto p:set){
-//             auto [i,j]=p;
-//             settingZeros(matrix,i,j);
-//         }
-        
-//         return;
+//         if(row)for(int j=0;j<n;j++)matrix[0][j]=0;
+//         if(col)for(int i=0;i<m;i++)matrix[i][0]=0;
         
         
-        bool row=false,col=false;
-        int m=matrix.size(),n=matrix[0].size();
-        
-        for(int i=0;i<m;i++){
-            if(matrix[i][0]==0){col=true;break;}
+        bool rowSet=false,colSet=false;
+        for(int i=0;i<matrix.size();i++){
+            if(matrix[i][0]==0){
+                colSet=true;break;
+            }
         }
-        for(int j=0;j<n;j++){
-            if(matrix[0][j]==0){row=true;break;}
+        for(int j=0;j<matrix[0].size();j++){
+            if(matrix[0][j]==0){
+                rowSet=true;break;
+            }
         }
-    
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
+        for(int i=1;i<matrix.size();i++){
+            for(int j=1;j<matrix[0].size();j++){
                 if(matrix[i][j]==0){
-                    matrix[i][0]=0;
-                    matrix[0][j]=0;
+                    matrix[i][0]=matrix[0][j]=0;
                 }
             }
         }
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                if(matrix[i][0]==0||matrix[0][j]==0){
+        for(int i=1;i<matrix.size();i++){
+            for(int j=1;j<matrix[0].size();j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
                     matrix[i][j]=0;
                 }
             }
         }
-        
-        if(row)for(int j=0;j<n;j++)matrix[0][j]=0;
-        if(col)for(int i=0;i<m;i++)matrix[i][0]=0;
-        
+        if(rowSet)for(int j=0;j<matrix[0].size();j++)matrix[0][j]=0;
+        if(colSet)for (int i=0;i<matrix.size();i++)matrix[i][0]=0;
     }
 };
