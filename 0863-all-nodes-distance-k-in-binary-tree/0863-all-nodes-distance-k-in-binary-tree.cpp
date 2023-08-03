25515429dd3c2889d4b6501b507b6083ca2135dd
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-04 01:38:53 +0530

    Time: 3 ms (93.20%), Space: 15.1 MB (16.73%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -10,42 +10,44 @@
 class Solution {
 public:
     
-   unordered_map<int,TreeNode*>pars; 
-    
-    void traverse(TreeNode*t,TreeNode*p){
-       if(!t)return;
-        pars[t->val]=p;
-        traverse(t->left,t);
-        traverse(t->right,t);
+    unordered_map<int,TreeNode*>par;
+    void setParent(TreeNode*t,TreeNode*prev){
+        if(!t)return;
+        par[t->val]=prev;
+        setParent(t->left,t);
+        setParent(t->right,t);
     }
     
-    void dfs(TreeNode*t,int k,unordered_set<TreeNode*>vis,vector<int>&res){
-        if(vis.find(t)!=vis.end())return;
-            vis.insert(t);
+    
+    unordered_set<int>vis;
+    vector<int>res;
+    
+    void dfs(TreeNode*t,int k){
+        
+        if(vis.find(t->val)!=vis.end())return;
+        vis.insert(t->val);
+
         if(k==0){
             res.push_back(t->val);
             return;
         }
+        
         if(t->left){
-            dfs(t->left,k-1,vis,res);
+            dfs(t->left,k-1);
         }
         if(t->right){
-            dfs(t->right,k-1,vis,res);
+            dfs(t->right,k-1);
         }
-        if(pars[t->val]){
-            dfs(pars[t->val],k-1,vis,res);
+        if(par[t->val]!=NULL){
+            dfs(par[t->val],k-1);
         }
-    }
-    
-    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        vector<int>res;
-        traverse(root,NULL);
-        unordered_set<TreeNode*>vis;
-        dfs(target,k,vis,res);
-        return res;
+        
         
     }
     
-    
-    
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        setParent(root,NULL);
+        dfs(target,k);
+        return res;
+    }
 };
