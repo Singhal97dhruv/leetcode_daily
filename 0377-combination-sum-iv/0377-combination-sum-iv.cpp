b9a4a5c598c23b3b9a92414e7d71dbdf45f55f91
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-05 02:14:33 +0530

    Time: 3 ms (48.12%), Space: 6.6 MB (77.47%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -15,10 +15,21 @@ public:
         // }
         // return dp[target];
         
-        vector<unsigned int>dp(target+1,0);
+        // vector<unsigned int>dp(target+1,0);
+        // dp[0]=1;
+        //  for(int i=1;i<=target;i++){
+        //     for(int j:nums){
+        //         if(j<=i){
+        //             dp[i]+=dp[i-j];
+        //         }
+        //     }
+        // }
+        // return dp[target];
+        
+        vector<uint32_t>dp(target+1,0);
         dp[0]=1;
-         for(int i=1;i<=target;i++){
-            for(int j:nums){
+        for(int i=1;i<=target;i++){
+            for(int j: nums){
                 if(j<=i){
                     dp[i]+=dp[i-j];
                 }
