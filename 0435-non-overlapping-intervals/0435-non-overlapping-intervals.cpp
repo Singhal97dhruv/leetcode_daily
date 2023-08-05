92470556be1c39bd1c63384f24b656a5648097c9
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-05 23:40:12 +0530

    Time: 307 ms (99.55%), Space: 89.8 MB (52.48%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,4 +1,4 @@
-bool cmp(vector<int>&a,vector<int>&b){return a[1]<b[1];}
+// bool cmp(vector<int>&a,vector<int>&b){return a[1]<b[1];}
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
@@ -18,15 +18,25 @@ public:
         int res=0;
         
         // sort(intervals.begin(),intervals.end(),[&](vector<int>x,vector<int>y){return x[1]<y[1];});
-        sort(intervals.begin(),intervals.end(),cmp);
-        int high=intervals[0][1];
+        // sort(intervals.begin(),intervals.end(),cmp);
+        // int high=intervals[0][1];
+        // for(int i=1;i<intervals.size();i++){
+        //     if(intervals[i][0]<high)res++;
+        //     else{
+        //         high=intervals[i][1];
+        //     }
+        // }
+        // return res;
+        
+        sort(intervals.begin(),intervals.end(),[](vector<int>&v1,vector<int>&v2){
+            return v1[1]<v2[1];
+        });
+            
+        int high= intervals[0][1];
         for(int i=1;i<intervals.size();i++){
             if(intervals[i][0]<high)res++;
-            else{
-                high=intervals[i][1];
-            }
+            else high= intervals[i][1];
         }
         return res;
-        
     }
 };
