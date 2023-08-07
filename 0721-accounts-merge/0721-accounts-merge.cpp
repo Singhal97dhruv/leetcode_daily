d49bd421ca1be3bf1229fed7c8d017d9524ce1ae
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-08 00:03:02 +0530

    Time: 81 ms (86.32%), Space: 42 MB (17.28%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.cpp
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -1,71 +1,67 @@
-class DSU{
-    vector<int>par,rank;
-    public:
-  DSU(int n):par(n),rank(n){
-      iota(par.begin(),par.end(),0);
-  }  
-    int find(int x){
-        if(par[x]==x)return x;
-        return par[x]=find(par[x]);
-    }
-    bool Union(int x,int y){
-        int xp=find(x);
-        int yp=find(y);
-        if(xp!=yp){
-            if(rank[xp]<rank[yp]){
-                par[xp]=yp;
-            }
-            else par[yp]=xp;
-            
-            rank[xp]+=rank[xp]==rank[yp];   
-            return true;
-            
-        }
-        return false;
-    }
-    
-};
-   
 class Solution {
+    
+    class DSU{
+        vector<int>par,rank;
+        public:
+            DSU(int n):par(n,0),rank(n,0){
+                iota(par.begin(),par.end(),0);
+            }
+            int find(int x){
+                if(par[x]==x)return x;
+                return par[x]=find(par[x]);
+            }
+            bool Union(int x,int y){
+                int xp=find(x);
+                int yp=find(y);
+                if(xp!=yp){
+                    if(rank[xp]<rank[yp]){
+                        par[xp]=yp;
+                    }
+                    else{
+                        par[yp]=xp;
+                    }
+                    rank[xp]+=rank[xp]==rank[yp];
+                    return true;
+                }
+                return false;
+            }
+    };
+    
 public:
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        
         DSU ds(accounts.size());
         
-        unordered_map<string,int>uf;
-        
+        unordered_map<string,int>map;
         for(int i=0;i<accounts.size();i++){
             for(int j=1;j<accounts[i].size();j++){
-                
-            
-            if(uf.find(accounts[i][j])!=uf.end()){
-                int par=uf[accounts[i][j]];
-                ds.Union(par,i);
-            }
-            else{
-                uf[accounts[i][j]]=i;
-            }
-            } 
-        }
-        
-        unordered_map<int,set<string>>emails;
-        
-        for(int i=0;i<accounts.size();i++){
-            int par=ds.find(i);
-            for(int j=1;j<accounts[i].size();j++){
-                emails[par].insert(accounts[i][j]);
+                if(map.find(accounts[i][j])!=map.end()){
+                    int par=map[accounts[i][j]];
+                    ds.Union(par,i);
+                }
+                else{
+                    map[accounts[i][j]]=i;
+                }
             }
         }
         
+        unordered_map<int,set<string>>emailsSet;
+        for(int i=0;i<accounts.size();i++){
+            int par= ds.find(i);
+            for(int j=1;j<accounts[i].size();j++){
+                emailsSet[par].insert(accounts[i][j]);
+            }
+        }
         vector<vector<string>>res;
-        
-        for(auto i:emails){
+        for(auto acc: emailsSet){
             vector<string>temp;
-            temp.push_back(accounts[i.first][0]);
-            // cout<<accounts[i.first][0];
-            for(auto j:i.second)temp.push_back(j);
+            temp.push_back(accounts[acc.first][0]);
+            
+            for(string email: acc.second){
+                temp.push_back(email);
+            }
             res.push_back(temp);
         }
-        return res;;
-        
+        return res;
     }
 };
