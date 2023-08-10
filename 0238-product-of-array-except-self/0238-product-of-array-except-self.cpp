4fe77fcd40f8a14040a84ad8be8fb2dfe504c7e7
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-10 22:56:19 +0530

    Time: 20 ms (83.20%), Space: 24 MB (59.19%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -45,19 +45,31 @@ public:
 //         }
 //         return res;
         
-        int n=nums.size();
-        vector<int>ans(n,1);
-        int temp=1;
-        for(int i=0;i<n;i++){
-            ans[i]*=temp;
-            temp*=nums[i];
-        }
-        temp=1;
-        for(int i=n-1;i>=0;i--){
-            ans[i]*=temp;
-            temp*=nums[i];
-        }
-        return ans;
+        // int n=nums.size();
+        // vector<int>ans(n,1);
+        // int temp=1;
+        // for(int i=0;i<n;i++){
+        //     ans[i]*=temp;
+        //     temp*=nums[i];
+        // }
+        // temp=1;
+        // for(int i=n-1;i>=0;i--){
+        //     ans[i]*=temp;
+        //     temp*=nums[i];
+        // }
+        // return ans;
         
+        int n=nums.size(),curr=1;
+        vector<int>arr(n,1);
+        for(int i=0;i<n;i++){
+            arr[i]*=curr;
+            curr*=nums[i];
+        }
+        curr=1;
+        for(int i=n-1;i>=0;i--){
+            arr[i]*=curr;
+            curr*=nums[i];
+        }
+        return arr;
     }
 };
