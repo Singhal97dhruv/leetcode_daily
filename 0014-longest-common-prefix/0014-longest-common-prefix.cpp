e301cd272200af99a60c32f06938c0e894970ad0
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-11 00:29:39 +0530

    Time: 0 ms (100.00%), Space: 9.2 MB (62.09%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -2,19 +2,30 @@ class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         
-        int n=INT_MAX;
-        for(string s:strs){
-            int sz=s.size();
-            n=min(n,sz);
-        }
-        string res="";
+        // int n=INT_MAX;
+        // for(string s:strs){
+        //     int sz=s.size();
+        //     n=min(n,sz);
+        // }
+        // string res="";
+        // for(int i=0;i<n;i++){
+        //     char c=strs[0][i];
+        //     for(int j=1;j<strs.size();j++){
+        //         if(strs[j][i]!=c)return res;
+        //     }
+        //     res+=c;
+        // }
+        // return res;
+        
+        int n=strs[0].size();
+        string ans="";
         for(int i=0;i<n;i++){
-            char c=strs[0][i];
+            char toCheck=strs[0][i];
             for(int j=1;j<strs.size();j++){
-                if(strs[j][i]!=c)return res;
+                if(i>=strs[j].size() || strs[j][i]!=toCheck)return ans;
             }
-            res+=c;
+            ans+=toCheck;
         }
-        return res;
+        return ans;
     }
 };
