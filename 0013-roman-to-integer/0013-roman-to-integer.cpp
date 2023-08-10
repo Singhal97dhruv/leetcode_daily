045536a37d8ed95498684ff9477db333b32988a7
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-11 00:09:55 +0530

    Time: 12 ms (66.96%), Space: 9 MB (5.95%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -24,14 +24,37 @@ public:
    
 //    return sum;
         
-        unordered_map<char,int>map={{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
-        int ans=map[s.back()];
-        for(int i=s.size()-2;i>=0;i--){
-            if(map[s[i]]<map[s[i+1]]){
-                ans-=map[s[i]];
+        // unordered_map<char,int>map={{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
+        // int ans=map[s.back()];
+        // for(int i=s.size()-2;i>=0;i--){
+        //     if(map[s[i]]<map[s[i+1]]){
+        //         ans-=map[s[i]];
+        //     }
+        //     else ans+=map[s[i]];
+        // }
+        // return ans;
+        
+        unordered_map<char,int>Roman={
+            {'I',1},
+            {'V',5},
+            {'X',10},
+            {'L',50},
+            {'C',100},
+            {'D',500},
+            {'M',1000}
+        };
+        int i=s.size(),ans=0;
+        while(i-1>=0){
+            if(Roman[s[i-1]]<Roman[s[i]]){
+                ans+=Roman[s[i]]-Roman[s[i-1]];
+                i-=2;
             }
-            else ans+=map[s[i]];
+            else{
+                ans+=Roman[s[i--]];
+            }
         }
+        if(i==0)ans+=Roman[s[0]];
         return ans;
+        
     }
 };
