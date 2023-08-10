41286aefd51c8b336fb58426e6eab7143e91f104
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-11 00:44:56 +0530

    Time: 16 ms (13.39%), Space: 47.4 MB (5.19%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -2,28 +2,38 @@ class Solution {
 public:
     string reverseWords(string s) {
         
-        stack<string>stk;
-        string tempWord="";
-        int i=0,n=s.size();
-        while(i<n){
-            if(s[i]==' '){
-                if(tempWord!=""){
-                    stk.push(tempWord);
-                    tempWord="";
-                }
-                while(i<n && s[i]==' ')i++;
-            }
-            else
-            tempWord+=s[i++];
-        }
-        if(tempWord!="")stk.push(tempWord);
+//         stack<string>stk;
+//         string tempWord="";
+//         int i=0,n=s.size();
+//         while(i<n){
+//             if(s[i]==' '){
+//                 if(tempWord!=""){
+//                     stk.push(tempWord);
+//                     tempWord="";
+//                 }
+//                 while(i<n && s[i]==' ')i++;
+//             }
+//             else
+//             tempWord+=s[i++];
+//         }
+//         if(tempWord!="")stk.push(tempWord);
         
-        string ans="";
-        while(stk.size()>1){
-            ans+=stk.top();stk.pop();
-            ans+=" ";
-        }
-        return ans+stk.top();
+//         string ans="";
+//         while(stk.size()>1){
+//             ans+=stk.top();stk.pop();
+//             ans+=" ";
+//         }
+//         return ans+stk.top();
         
+        stringstream ss(s);
+        vector<string>vect;
+        string ans="",temp;
+        while(ss>>temp){
+            vect.push_back(temp);
+        }
+        for(int i=vect.size()-1;i>=1;i--){
+            ans=ans+vect[i]+" ";
+        }
+        return ans+vect[0];
     }
 };
