096e86a07fc9de1e475e86fdc90712179632266b
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-11 22:56:36 +0530

    Time: 8 ms (32.16%), Space: 7.3 MB (66.02%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -32,16 +32,26 @@ public:
 //         return true;
         
         
+//         int i=0,j=s.size()-1;
+//         while(i<j){
+//             if(!isalnum(s[i]))i++;
+//             else if(!isalnum(s[j]))j--;
+           
+//             else if(tolower(s[i])==tolower(s[j]))i++,j--;
+//             else return false;
+//         }
+//         return true;
+        
+        
         int i=0,j=s.size()-1;
         while(i<j){
             if(!isalnum(s[i]))i++;
             else if(!isalnum(s[j]))j--;
-           
+            
             else if(tolower(s[i])==tolower(s[j]))i++,j--;
             else return false;
         }
         return true;
-        
         
     }
 };
