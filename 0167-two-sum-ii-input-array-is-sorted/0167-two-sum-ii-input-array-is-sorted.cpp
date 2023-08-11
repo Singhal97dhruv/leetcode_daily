c3f368ebbdefb0f55427152d56f176838e8a03bd
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-11 23:26:49 +0530

    Time: 12 ms (63.71%), Space: 15.6 MB (28.39%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -26,12 +26,27 @@ public:
         // }
         // return{};
         
-        unordered_map<int,int>map;
-        for(int i=0;i<numbers.size();i++){
-            if(map.find(target-numbers[i])!=map.end()){
-                return {map[target-numbers[i]]+1,i+1};
+        // unordered_map<int,int>map;
+        // for(int i=0;i<numbers.size();i++){
+        //     if(map.find(target-numbers[i])!=map.end()){
+        //         return {map[target-numbers[i]]+1,i+1};
+        //     }
+        //     map[numbers[i]]=i;
+        // }
+        // return {};
+        
+        int left=0,right=numbers.size()-1;
+        while(left<right){
+            if(numbers[left]+numbers[right]==target){
+                return {left+1,right+1};
             }
-            map[numbers[i]]=i;
+            else if(numbers[left]+numbers[right]>target){
+                right--;
+            }
+            
+            else {
+                left++;
+            }
         }
         return {};
     }
