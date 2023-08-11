593172b0d8a855cfd6c33470a7239362b6bfc2ef
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-11 23:58:38 +0530

    Time: 121 ms (87.53%), Space: 24.1 MB (64.70%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -24,28 +24,51 @@ public:
 //         }
         
 //         return res;
+//         int n=nums.size();
+//      sort(nums.begin(),nums.end());
+//         vector<vector<int>>res;
+//         for(int i=0;i<n-2;i++){
+//             int target=-nums[i];
+//             int left=i+1,right=n-1;
+//             while(left<right){
+//                 int sum=nums[left]+nums[right];
+//                 if(sum==target){
+//                     res.push_back({nums[i],nums[left],nums[right]});
+                    
+//                     while(left<right && nums[left]==nums[left+1])left++;
+//                     while(left<right && nums[right]==nums[right-1])right--;
+//                     left++,right--;
+                    
+//                 }
+//                 else if(target<sum){
+//                     right--;
+//                 }
+//                 else left++;
+//             }
+//             while(i<n-2 && nums[i]==nums[i+1])i++;
+//         }
+//         return res;
+        
         int n=nums.size();
-     sort(nums.begin(),nums.end());
         vector<vector<int>>res;
+        sort(nums.begin(),nums.end());
         for(int i=0;i<n-2;i++){
             int target=-nums[i];
             int left=i+1,right=n-1;
             while(left<right){
-                int sum=nums[left]+nums[right];
-                if(sum==target){
+                if(nums[left]+nums[right]==target){
                     res.push_back({nums[i],nums[left],nums[right]});
-                    
                     while(left<right && nums[left]==nums[left+1])left++;
                     while(left<right && nums[right]==nums[right-1])right--;
                     left++,right--;
-                    
+
                 }
-                else if(target<sum){
-                    right--;
-                }
-                else left++;
+                else if(nums[left]+nums[right]<target)left++;
+                else right--;
             }
+        
             while(i<n-2 && nums[i]==nums[i+1])i++;
+            
         }
         return res;
     }
