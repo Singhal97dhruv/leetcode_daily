d38f69b865b2bb700b3d821921200269357d403d
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-14 22:52:51 +0530

    Time: 4 ms (35.13%), Space: 7 MB (47.78%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -26,14 +26,28 @@ public:
 //        }
 //         return ;
         
-        for(int i=0;i<matrix.size();i++){
+        // for(int i=0;i<matrix.size();i++){
+        //     for(int j=0;j<i;j++){
+        //         swap(matrix[i][j],matrix[j][i]);
+        //     }
+        // }
+        // for(int i=0;i<matrix.size();i++){
+        //     for(int j=0;j<matrix.size()/2;j++){
+        //         swap(matrix[i][j],matrix[i][matrix.size()-j-1]);
+        //     }
+        // }
+        
+        int m=matrix.size(),n=matrix[0].size();
+        
+        for(int i=0;i<m;i++){
             for(int j=0;j<i;j++){
                 swap(matrix[i][j],matrix[j][i]);
             }
         }
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix.size()/2;j++){
-                swap(matrix[i][j],matrix[i][matrix.size()-j-1]);
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n/2;j++){
+                swap(matrix[i][j],matrix[i][n-j-1]);
             }
         }
     }
