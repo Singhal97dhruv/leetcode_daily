b21c1bbf2c3044b9ff1f6b77dd84cdad00605e2b
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-14 22:47:44 +0530

    Time: 0 ms (100.00%), Space: 7 MB (22.86%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -32,44 +32,42 @@ public:
 //         }
 //         return res;
         
-        
-        vector<int>res;
-        int top=0,left=0,right=matrix[0].size()-1,bottom=matrix.size()-1;
-        
-        int dirn=0;
-        
+        int m=matrix.size(),n=matrix[0].size();
+        int left=0,top=0,right=m-1,bottom=n-1,dirn=0;
+        vector<int>ans;
         while(left<=right && top<=bottom){
             
             if(dirn==0){
-                for(int i=left;i<=right;i++){
-                    res.push_back(matrix[top][i]);
-                }
-            top++;
-            }
-            else if(dirn==1){
                 for(int i=top;i<=bottom;i++){
-                    res.push_back(matrix[i][right]);
-                }
-                right--;
-            }
-            else if(dirn==2){
-                for(int i=right;i>=left;i--){
-                    res.push_back(matrix[bottom][i]);
-                }
-                bottom--;
-            }
-            else {
-                for(int i=bottom;i>=top;i--){
-                    res.push_back(matrix[i][left]);
+                    ans.push_back(matrix[left][i]);
                 }
                 left++;
             }
+            
+            else if(dirn==1){
+                for(int j=left;j<=right;j++){
+                    ans.push_back(matrix[j][bottom]);
+                }
+                bottom--;
+            }
+            
+            else if(dirn==2){
+                for(int i=bottom;i>=top;i--){
+                    ans.push_back(matrix[right][i]);
+                }
+                right--;
+            }
+            else{
+                for(int j=right;j>=left;j--){
+                    ans.push_back(matrix[j][top]);
+                }
+                top++;
+            }
+            
             dirn=(dirn+1)%4;
             
         }
-        return res;
-        
-        
+        return ans;
         
         
         
