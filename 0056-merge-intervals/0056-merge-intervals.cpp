ec5c1581775a0a3245c1befb969ba183a1133322
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-15 23:11:56 +0530

    Time: 34 ms (75.79%), Space: 19 MB (70.81%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -14,18 +14,31 @@ public:
 //         return res;
         
         
-       sort(intervals.begin(),intervals.end());
+       // sort(intervals.begin(),intervals.end());
         
-        vector<vector<int>>res;
+//         vector<vector<int>>res;
+//         res.push_back(intervals[0]);
+//         for(int i=1;i<intervals.size();i++){
+//             if(res.back()[1]<intervals[i][0]){
+//                 res.push_back(intervals[i]);
+//             }
+//             else{
+//                 res.back()[1]=max(intervals[i][1],res.back()[1]);
+//             }
+//         }
+//         return res;
+    vector<vector<int>>res;
+    sort(intervals.begin(),intervals.end());
         res.push_back(intervals[0]);
         for(int i=1;i<intervals.size();i++){
-            if(res.back()[1]<intervals[i][0]){
+            if(intervals[i][0]>res.back()[1]){
                 res.push_back(intervals[i]);
             }
             else{
-                res.back()[1]=max(intervals[i][1],res.back()[1]);
+                res.back()[1]=max(res.back()[1],intervals[i][1]);
             }
         }
-        return res;
+    return res;
     }
+    
 };
