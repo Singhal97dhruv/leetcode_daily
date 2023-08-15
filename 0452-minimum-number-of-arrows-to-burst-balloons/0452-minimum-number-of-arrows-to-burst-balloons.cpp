fa3ff13f146c36e35c712afd11f002c8b3b75db6
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-15 23:54:14 +0530

    Time: 358 ms (85.11%), Space: 89.9 MB (28.89%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,15 +1,24 @@
- bool cmp(vector<int>&a,vector<int>&b ){return a[1]<b[1];}
+ // bool cmp(vector<int>&a,vector<int>&b ){return a[1]<b[1];}
 class Solution {
 public:
     int findMinArrowShots(vector<vector<int>>& points) {
-        int cnt=0,high=0;
-        sort(points.begin(),points.end(),cmp);
-        for(int i=0;i<points.size();i++){
-            if(cnt==0||points[i][0]>high ){
-                cnt++;
-                high=points[i][1];
-            }
+//         int cnt=0,high=0;
+//         sort(points.begin(),points.end(),cmp);
+        
+//         for(int i=0;i<points.size();i++){
+//             if(cnt==0||points[i][0]>high ){
+//                 cnt++;
+//                 high=points[i][1];
+//             }
+//         }
+//         return cnt;
+        
+        sort(points.begin(),points.end(),[](vector<int>&v1,vector<int>&v2){return v1[1]<v2[1];});
+        
+        int ans=1,high=points[0][1];
+        for(int i=1;i<points.size();i++){
+            if(points[i][0]>high){ans++;high=points[i][1];}
         }
-        return cnt;
+        return ans;
     }
 };
