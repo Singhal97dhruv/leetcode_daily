41a5c33e50f141d6793779e189b20813623ff679
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-15 23:06:04 +0530

    Time: 3 ms (34.94%), Space: 6.9 MB (15.51%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.cpp
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -2,17 +2,28 @@ class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
      
-        int size=nums.size();
-        vector<string>s;
-        if(size==0)return s;
-        for(int i=0;i<size;){
-            int start=i,end=i;
-            while(end+1<size &&nums[end]+1==nums[end+1])end++;
-        if(end>start)s.push_back(to_string(nums[start])+"->"+to_string(nums[end]));
-            else{s.push_back(to_string(nums[start]));}
-         i=end+1;   
-        }
+//         int size=nums.size();
+//         vector<string>s;
+//         if(size==0)return s;
+//         for(int i=0;i<size;){
+//             int start=i,end=i;
+//             while(end+1<size &&nums[end]+1==nums[end+1])end++;
+//         if(end>start)s.push_back(to_string(nums[start])+"->"+to_string(nums[end]));
+//             else{s.push_back(to_string(nums[start]));}
+//          i=end+1;   
+//         }
         
-  return s;      
+//   return s;     
+        int n=nums.size();
+        vector<string>ans;
+        for(int end=0;end<n;end++){
+            int i=end;
+            while(i+1<n && nums[i]+1==nums[i+1])i++;
+            if(i==end)ans.push_back(to_string(nums[i]));
+            else
+                ans.push_back(to_string(nums[end])+"->"+to_string(nums[i]));
+            end=i;
+        }
+        return ans;
     }
 };
