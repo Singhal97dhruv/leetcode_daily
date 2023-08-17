07f3a6befc033052b4b90fb7425f1301a7a6f045
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-18 02:02:13 +0530

    Time: 63 ms (89.53%), Space: 30.2 MB (86.89%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -27,15 +27,33 @@ public:
 //         return mat;
         
        
-        vector<int>offset{0,1,0,-1,0};
+//         vector<int>offset{0,1,0,-1,0};
+//         queue<pair<int,int>>q;
+//         for(int i=0;i<mat.size();i++){
+//             for(int j=0;j<mat[0].size();j++){
+//                 if(mat[i][j]==0)q.push({i,j});
+//                 else mat[i][j]=-1;
+//             }
+//         }
+        
+//         while(!q.empty()){
+//             auto [i,j]=q.front();q.pop();
+//             for(int k=0;k<4;k++){
+//                 int nr=i+offset[k],nc=j+offset[k+1];
+//                 if(nr<0 || nc<0 || nr>=mat.size() || nc>=mat[0].size() || mat[nr][nc]!=-1)continue;
+//                 mat[nr][nc]=mat[i][j]+1;
+//                 q.push({nr,nc});
+//             }
+//         }
+//         return mat;
         queue<pair<int,int>>q;
+        vector<int>offset={0,1,0,-1,0};
         for(int i=0;i<mat.size();i++){
             for(int j=0;j<mat[0].size();j++){
                 if(mat[i][j]==0)q.push({i,j});
                 else mat[i][j]=-1;
             }
         }
-        
         while(!q.empty()){
             auto [i,j]=q.front();q.pop();
             for(int k=0;k<4;k++){
@@ -46,6 +64,26 @@ public:
             }
         }
         return mat;
-        
     }
 };
+
+
+int dp[200001]={[0 ... 200000]=-1};
+int recur(int curr,int tar,int x,int y,int z){
+    
+    if(curr==tar)return 0;
+    if(dp[curr]!=-1)return dp[curr];
+    if(curr>tar){
+        return z+recur(curr-1,tar,x,y,z);
+    }
+    
+    int ans=0;
+    
+    return min({x+recur(2*curr,tar,x,y,z),y+recur(curr+1,tar,x,y,z)});
+    
+    
+    
+    
+}
+    
+
