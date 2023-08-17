f890ae734d5af058655ac30bb2372cd5db9fa554
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-18 00:45:42 +0530

    Time: 0 ms (100.00%), Space: 10.4 MB (11.83%) - LeetHub

--- 0086-partition-list/0086-partition-list.cpp
+++ 0086-partition-list/0086-partition-list.cpp
@@ -12,25 +12,40 @@ class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
         
-        ListNode*p1=new ListNode(0);
-        ListNode*p2=new ListNode(0);
-        ListNode*p3=p2,*p4=p1;
+//         ListNode*p1=new ListNode(0);
+//         ListNode*p2=new ListNode(0);
+//         ListNode*p3=p2,*p4=p1;
+//         while(head){
+//             if(head->val<x){
+//                 p1->next=head;
+//                 p1=p1->next;
+                
+//             }
+//             else {
+//                 p2->next=head;
+//                 p2=p2->next;
+                
+//             }
+//             head=head->next;
+//         }
+//         p1->next=p3->next;
+//         p2->next=NULL;
+//         return p4->next;
+        
+        ListNode*p1=new ListNode(0),*p2=new ListNode(0),*p3=p1,*p4=p2;
         while(head){
             if(head->val<x){
-                p1->next=head;
+                p1->next=new ListNode(head->val);
                 p1=p1->next;
-                
             }
-            else {
-                p2->next=head;
+            else{
+                p2->next=new ListNode(head->val);
                 p2=p2->next;
-                
             }
             head=head->next;
         }
-        p1->next=p3->next;
-        p2->next=NULL;
-        return p4->next;
+        p1->next=p4->next;
+        return p3->next;
         
         
     }
