5b01932cf175c2aa8099fdc734a5bf971373e4a3
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-18 01:00:10 +0530

    Time: 357 ms (98.45%), Space: 165 MB (77.24%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,5 +1,9 @@
 class LRUCache {
 public:
+    // unordered_map<int,list<pair<int,int>>::iterator>map;
+    // list<pair<int,int>>l1;
+    // int cap;
+    
     unordered_map<int,list<pair<int,int>>::iterator>map;
     list<pair<int,int>>l1;
     int cap;
@@ -10,24 +14,44 @@ public:
     
     int get(int key) {
         
+        // if(map.find(key)==map.end())return -1;
+        // l1.splice(l1.begin(),l1,map[key]);
+        // return map[key]->second;
+        
         if(map.find(key)==map.end())return -1;
+        
         l1.splice(l1.begin(),l1,map[key]);
         return map[key]->second;
+        
     }
     
     void put(int key, int value) {
         
+        // if(get(key)!=-1){
+        //     map[key]->second=value;
+        //     return;
+        // }        
+        // while(map.size()==cap){
+        //     int delKey= l1.back().first;
+        //     l1.pop_back();
+        //     map.erase(delKey);
+        // }
+        // l1.emplace_front(key,value);
+        // map[key]=l1.begin();
+        
         if(get(key)!=-1){
             map[key]->second=value;
             return;
-        }        
-        while(map.size()==cap){
-            int delKey= l1.back().first;
+        } 
+        if(map.size()==cap){
+            int delKey=l1.back().first;
             l1.pop_back();
             map.erase(delKey);
         }
         l1.emplace_front(key,value);
         map[key]=l1.begin();
+        
+        
         
     }
 };
