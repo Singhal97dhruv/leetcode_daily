3fe313b294f57e87d5395d7301bdc08684b698ab
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-19 01:29:58 +0530

    Time: 4 ms (99.14%), Space: 17.4 MB (50.33%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.cpp
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.cpp
@@ -19,34 +19,34 @@ public:
 class Solution {
 public:
     
-    Node*helper(Node*R){
-        if(!R)return NULL;
-        if(R->left)return R->left;
-        if(R->right)return R->right;
+//     Node*helper(Node*R){
+//         if(!R)return NULL;
+//         if(R->left)return R->left;
+//         if(R->right)return R->right;
         
-        return helper(R->next);
+//         return helper(R->next);
         
-    }
+//     }
     
-    void connectNexts(Node*root){
-        if(!root)return;
-         if(root->left)
-        {
-            if(root->right){
-                root->left->next=root->right;
-            }
-            else{
-                root->left->next=helper(root->next);
-            }
-        }
+//     void connectNexts(Node*root){
+//         if(!root)return;
+//          if(root->left)
+//         {
+//             if(root->right){
+//                 root->left->next=root->right;
+//             }
+//             else{
+//                 root->left->next=helper(root->next);
+//             }
+//         }
         
-        if(root->right){
-            root->right->next=helper(root->next);
-        }
+//         if(root->right){
+//             root->right->next=helper(root->next);
+//         }
         
-        connectNexts(root->right);
-        connectNexts(root->left);
-    }
+//         connectNexts(root->right);
+//         connectNexts(root->left);
+//     }
     
     Node* connect(Node* root) {
         // Node*now=root,*head=NULL,*tail=NULL;
@@ -65,8 +65,28 @@ public:
         //     }
         // }
         // return root;
-        connectNexts(root);
-      return root;
         
+        
+        
+      //   connectNexts(root);
+      // return root;
+        
+        
+        Node*now=root,*head=NULL,*tail=NULL;
+        while(now){
+            if(now->left){
+                if(tail)tail=tail->next=now->left;
+                else head=tail=now->left;
+            }
+            if(now->right){
+                if(tail)tail=tail->next=now->right;
+                else head=tail=now->right;
+            }
+            if(!(now=now->next)){
+                now=head;
+                head=tail=NULL;
+            }
+        }
+        return root;
     }
 };
