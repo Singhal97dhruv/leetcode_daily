39b120119a8e060abfd47592135daab1885bc91e
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-22 13:25:06 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (44.02%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,76 +1,30 @@
 class Solution {
 public:
-//     const vector<string>pad={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     
     vector<string>offset={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     
-//     vector<string>res;
+    vector<string>ans;
     
-//     void recur(string s,int st,string dig){
-//         if(st==dig.size()){
-//             res.push_back(s);
-//             return;
-//         }
-//         int idx=dig[st]-'0';
-//         int n=offset[idx].size();
-        
-//         for(int i=0;i<n;i++){
-//             s+=offset[idx][i];
-//             recur(s,st+1,dig);
-//             s.pop_back();
-//         }
-        
-        
-//     }
-    
-    vector<string>res;
-    
-//     void backtrack(int idx,string curr,string digits){
-//         if(idx>=digits.size()){
-//             res.push_back(curr);
-//             return ;
-//         }
-//         int offsetIdx=digits[idx]-'0';
-        
-//         for(int i=0;i<offset[offsetIdx].size();i++){
-//             backtrack(idx+1,curr+offset[offsetIdx][i],digits);
-//         }
-        
-//     }
-    
-    void backtrack(int idx,string digits,string curr){
+    void backtrack(int idx,string s,string digits){
         if(idx>=digits.size()){
-            res.push_back(curr);
-            return;
+            ans.push_back(s);
+            return ;
         }
         
-        int offsetIdx=digits[idx]-'0';
-        for(int i=0;i<offset[offsetIdx].size();i++){
-            backtrack(idx+1,digits,curr+offset[offsetIdx][i]);
-        }
+        // for(int i=idx;i<digits.size();i++){
+            for(char j:offset[digits[idx]-'0']){
+                s+=j;
+                backtrack(idx+1,s,digits);
+                s.pop_back();
+            }
+        // }
         
     }
     
+    
     vector<string> letterCombinations(string digits) {
-//         if(digits.empty())return {};
-//         vector<string>result;
-//         result.push_back("");
-//         for(auto nums:digits){
-//             vector<string>temp;
-//             for(auto can:pad[nums-'0'])
-//                 for(auto s:result)
-//                     temp.push_back(s+can);
-//         swap(result,temp);
-//         }
-        
-//         return result;
-        
         if(digits.size()==0)return {};
-        // recur("",0,digits);
-        
-        // backtrack(0,"",digits);
-        backtrack(0,digits,"");
-        return res;
-        
+        backtrack(0,"",digits);
+        return ans;
     }
 };
