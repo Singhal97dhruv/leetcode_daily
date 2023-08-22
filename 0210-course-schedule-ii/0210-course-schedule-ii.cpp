4e1291d41b80f6056bbad908ce5b2a1f3373ce0a
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-22 12:59:27 +0530

    Time: 22 ms (56.07%), Space: 13.7 MB (64.46%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,35 +1,48 @@
 class Solution {
 public:
     
-    bool isCycle(int u,vector<int>adjList[],vector<int>&vis,vector<int>&ans){
-        if(vis[u]==2)return false;
-        if(vis[u]==1)return true;
+//     bool isCycle(int u,vector<int>adjList[],vector<int>&vis,vector<int>&ans){
+//         if(vis[u]==2)return false;
+//         if(vis[u]==1)return true;
         
+//         vis[u]=1;
+//         for(int v:adjList[u]){
+//             if(isCycle(v,adjList,vis,ans))return true;
+//         }
+//         vis[u]=2;
+//         ans.push_back(u);
+//         return false;
+        
+//     }
+    
+    bool isCycle(int u,vector<int>&vis,vector<int>adj[],vector<int>&ans){
+        if(vis[u]==1)return true;
+        if(vis[u]==2)return false;
         vis[u]=1;
-        for(int v:adjList[u]){
-            if(isCycle(v,adjList,vis,ans))return true;
+        for(int v:adj[u]){
+            if(isCycle(v,vis,adj,ans))return true;
         }
         vis[u]=2;
         ans.push_back(u);
         return false;
-        
     }
+    
     
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         
-        vector<int>adjList[numCourses];
-        vector<int>ans;
+//         vector<int>adjList[numCourses];
+//         vector<int>ans;
         
-        for(int i=0;i<prerequisites.size();i++){
-            adjList[prerequisites[i][1]].push_back(prerequisites[i][0]);
-            // indegree[prerequisites[i][0]]++;
-        }
+//         for(int i=0;i<prerequisites.size();i++){
+//             adjList[prerequisites[i][1]].push_back(prerequisites[i][0]);
+//             // indegree[prerequisites[i][0]]++;
+//         }
         
-        vector<int>vis(numCourses,0);
-        for(int i=0;i<numCourses;i++){
-            if(isCycle(i,adjList,vis,ans))return {};
-        }
-        reverse(ans.begin(),ans.end());
+//         vector<int>vis(numCourses,0);
+//         for(int i=0;i<numCourses;i++){
+//             if(isCycle(i,adjList,vis,ans))return {};
+//         }
+//         reverse(ans.begin(),ans.end());
         
         
 //         for(int i=0;i<numCourses;i++){
@@ -49,6 +62,20 @@ public:
 //             }
             
 //         }
+        // return ans;
+        
+        
+        vector<int>adj[numCourses];
+        vector<int>vis(numCourses);
+        vector<int>ans;
+        for(int i=0;i<prerequisites.size();i++){
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        }
+        
+        for(int i=0;i<numCourses;i++){
+            if(isCycle(i,vis,adj,ans))return {};
+        }
+        reverse(ans.begin(),ans.end());
         return ans;
         
         
