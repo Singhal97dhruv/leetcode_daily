0a893b21a00c480ffc099a9e1f0b9da200d94759
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-24 01:06:39 +0530

    Time: 3 ms (70.65%), Space: 15.2 MB (29.93%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -11,42 +11,40 @@ public:
 //         if(m>0)
 //             genpar(paren,st+")",n,m-1);
 //     }
-    
-    
-//     void recur(string s,int n,int m,vector<string>&ans){
-        
-//         if(n==0 && m==0){
-//             ans.push_back(s);
-//             return;
-//         }
-        
-//         if(n>0){
-//             recur(s+"(",n-1,m+1,ans);
-            
-//         }
-//         if(m>0){
-//             recur(s+")",n,m-1,ans);
-//         }
-    
-//     }
-    
     vector<string>res;
-    void recur(string curr,int leftPths, int rightPths){
-        
-        if(leftPths==0 && rightPths==0)
-        {
-            res.push_back(curr);
+    void GenParenthesis(int n1,int n2,string pattern){
+        if(n1==0 && n2==0){
+            res.push_back(pattern);
             return ;
         }
-            
-        if(leftPths>0){
-            recur(curr+'(',leftPths-1,rightPths+1);
+        
+        if(n1>0){
+            GenParenthesis(n1-1,n2+1,pattern+'(');
         }
-        if(rightPths>0){
-            recur(curr+')',leftPths,rightPths-1);
+        
+        if(n2>0){
+            GenParenthesis(n1,n2-1,pattern+')');
         }
         
     }
+    
+    // vector<string>res;
+//     void recur(string curr,int leftPths, int rightPths){
+        
+//         if(leftPths==0 && rightPths==0)
+//         {
+//             res.push_back(curr);
+//             return ;
+//         }
+            
+//         if(leftPths>0){
+//             recur(curr+'(',leftPths-1,rightPths+1);
+//         }
+//         if(rightPths>0){
+//             recur(curr+')',leftPths,rightPths-1);
+//         }
+        
+//     }
     
     
     
@@ -58,7 +56,8 @@ public:
         
         
         // vector<string>res;
-        recur("",n,0);
+        // recur("",n,0);
+        GenParenthesis(n,0,"");
         return res;
         
         
