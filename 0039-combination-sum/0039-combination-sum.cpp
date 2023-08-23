c500bf1e7b11bad5755733c661b934bb6bdd869c
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-24 00:08:40 +0530

    Time: 9 ms (44.97%), Space: 11 MB (48.55%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,25 +1,50 @@
 class Solution {
 public:
     
-    vector<vector<int>>res;
+//     vector<vector<int>>res;
     
-    void combination(int currSum,int idx,vector<int>&candidates,int target,vector<int>&t){
-        if(currSum>target)return;
-        if(currSum==target){res.push_back(t);return;}
+//     void combination(int currSum,int idx,vector<int>&candidates,int target,vector<int>&t){
+//         if(currSum>target)return;
+//         if(currSum==target){res.push_back(t);return;}
+        
+//         for(int i=idx;i<candidates.size();i++){
+//             t.push_back(candidates[i]);
+//             combination(currSum+candidates[i],i, candidates,target,t);
+//             t.pop_back();
+//         }
+        
+//     }
+    
+    vector<vector<int>>res;
+
+    void recur(int idx,vector<int>&temp,int sum,vector<int>&candidates,int target){
+        if(sum>target)return;
+        if(sum==target){
+            res.push_back(temp);
+            return;
+        }
         
         for(int i=idx;i<candidates.size();i++){
-            t.push_back(candidates[i]);
-            combination(currSum+candidates[i],i, candidates,target,t);
-            t.pop_back();
+            
+            temp.push_back(candidates[i]);
+            recur(i,temp,sum+candidates[i],candidates,target);
+            temp.pop_back();
+            
         }
+        
+        
         
     }
     
+    
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         
-        vector<int>t;
+//         vector<int>t;
         
-        combination(0,0,candidates,target,t);
+//         combination(0,0,candidates,target,t);
+        
+        vector<int>temp;
+        recur(0,temp,0,candidates,target);
         
         return res;
         
