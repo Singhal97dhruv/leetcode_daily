1ead30568b13d8a93ed2182c953ca01e40eab241
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-24 00:01:06 +0530

    Time: 5 ms (29.03%), Space: 8.1 MB (18.50%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -20,15 +20,28 @@ public:
     
 //     }
     
-    void permute(int st,vector<int>&nums){
-        if(st==nums.size()){
+//     void permute(int st,vector<int>&nums){
+//         if(st==nums.size()){
+//             res.push_back(nums);return;
+//         }
+        
+//         for(int i=st;i<nums.size();i++){
+//             swap(nums[i],nums[st]);
+//             permute(st+1,nums);
+//             swap(nums[i],nums[st]);
+//         }
+        
+//     }
+    
+    void permute(vector<int>&nums,int idx){
+        if(idx>=nums.size()){
             res.push_back(nums);return;
         }
         
-        for(int i=st;i<nums.size();i++){
-            swap(nums[i],nums[st]);
-            permute(st+1,nums);
-            swap(nums[i],nums[st]);
+        for(int i=idx;i<nums.size();i++){
+            swap(nums[i],nums[idx]);
+            permute(nums,idx+1);
+            swap(nums[i],nums[idx]);
         }
         
     }
@@ -36,7 +49,7 @@ public:
     
     vector<vector<int>> permute(vector<int>& nums) {
         
-        permute(0,nums);
+        permute(nums,0);
         return res;
         
     }
