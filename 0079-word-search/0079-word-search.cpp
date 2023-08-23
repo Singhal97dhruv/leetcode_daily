c3932724fcbbe61b20a95597af2e03ff0bbe48f6
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-24 01:18:24 +0530

    Time: 964 ms (27.56%), Space: 7.9 MB (73.92%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,15 +1,30 @@
 class Solution {
 public:
     vector<int>offset{0,1,0,-1,0};
-    bool dfs(int i,int j,int idx, vector<vector<char>>&board,string word){
+//     bool dfs(int i,int j,int idx, vector<vector<char>>&board,string word){
+//         if(idx>=word.size())return true;
+//         if(i<0 || j<0 || i>=board.size() || j>=board[0].size() || board[i][j]=='*' || board[i][j]!=word[idx])return false;
+        
+//         char c=board[i][j];
+//         board[i][j]='*';
+//         bool ans=false;
+//         for(int k=0;k<4;k++){
+//             ans= ans|| dfs(i+offset[k],j+offset[k+1],idx+1,board,word);
+//         }
+//         board[i][j]=c;
+//         return ans;
+        
+        
+//     }
+    
+    bool dfs(int i,int j,int idx,vector<vector<char>>&board,string word){
         if(idx>=word.size())return true;
         if(i<0 || j<0 || i>=board.size() || j>=board[0].size() || board[i][j]=='*' || board[i][j]!=word[idx])return false;
-        
         char c=board[i][j];
         board[i][j]='*';
         bool ans=false;
         for(int k=0;k<4;k++){
-            ans= ans|| dfs(i+offset[k],j+offset[k+1],idx+1,board,word);
+           ans=ans||dfs(i+offset[k],j+offset[k+1],idx+1,board,word);
         }
         board[i][j]=c;
         return ans;
