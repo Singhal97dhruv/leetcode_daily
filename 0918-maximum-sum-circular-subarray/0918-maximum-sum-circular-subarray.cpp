a9e2279d8821e685fabd7abf6d9ec137eb34fc43
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-25 00:11:03 +0530

    Time: 55 ms (60.48%), Space: 39.8 MB (48.66%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -1,5 +1,16 @@
 class Solution {
 public:
+    
+    int kadane(vector<int>&nums){
+        int mx=nums[0],currSum=nums[0];
+        for(int i=1;i<nums.size();i++){
+            if(currSum<0)currSum=0;
+            currSum+=nums[i];
+            mx=max(mx,currSum);
+        }
+        return mx;
+    }
+    
     int maxSubarraySumCircular(vector<int>& nums) {
         
         int nonCircular=kadane(nums);
@@ -14,15 +25,15 @@ public:
         
     }
     
-    int kadane(vector<int>v){
-        int mx=v[0],curr_sum=v[0];
-        for(int i=1;i<v.size();i++){
-            if(curr_sum<0)
-                curr_sum=0;
-            curr_sum+=v[i];
-            mx=max(mx,curr_sum);
-        }
-        return mx;
-    }
+    // int kadane(vector<int>v){
+    //     int mx=v[0],curr_sum=v[0];
+    //     for(int i=1;i<v.size();i++){
+    //         if(curr_sum<0)
+    //             curr_sum=0;
+    //         curr_sum+=v[i];
+    //         mx=max(mx,curr_sum);
+    //     }
+    //     return mx;
+    // }
     
 };
