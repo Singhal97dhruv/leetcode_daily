4972e8dae986fd382dc6d0d2f4b640b6bb1d920d
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-08-25 01:05:00 +0530

    Time: 292 ms (47.95%), Space: 80.5 MB (35.94%) - LeetHub

--- 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.cpp
+++ 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.cpp
@@ -1,29 +1,44 @@
 class Solution {
 public:
     
-    int dp[51][51];
-    int recur(int idx,int J,vector<vector<int>>&grid,vector<vector<int>>&moveCost){
+    // int dp[51][51];
+//     int recur(int idx,int J,vector<vector<int>>&grid,vector<vector<int>>&moveCost){
         
-        if(idx>=grid.size())return 0;
-        if(dp[idx][J]!=-1)return dp[idx][J];
-        int ans=INT_MAX;
-        int prev=grid[idx-1][J];
-        for(int j=0;j<grid[0].size();j++){
-            ans=min(ans,grid[idx][j]+moveCost[prev][j]+recur(idx+1,j,grid,moveCost));
-        }
-        return dp[idx][J]= ans;
+//         if(idx>=grid.size())return 0;
+//         if(dp[idx][J]!=-1)return dp[idx][J];
+//         int ans=INT_MAX;
+//         int prev=grid[idx-1][J];
+//         for(int j=0;j<grid[0].size();j++){
+//             ans=min(ans,grid[idx][j]+moveCost[prev][j]+recur(idx+1,j,grid,moveCost));
+//         }
+//         return dp[idx][J]= ans;
         
-    }
+//     }
     
     
     int minPathCost(vector<vector<int>>& grid, vector<vector<int>>& moveCost) {
         
-        int ans=INT_MAX;
-        memset(dp,-1,sizeof(dp));
-        for(int j=0;j<grid[0].size();j++){
-            ans=min(ans,grid[0][j]+recur(1,j,grid,moveCost));
-        }
+//         int ans=INT_MAX;
+//         memset(dp,-1,sizeof(dp));
+//         for(int j=0;j<grid[0].size();j++){
+//             ans=min(ans,grid[0][j]+recur(1,j,grid,moveCost));
+//         }
         
-        return ans;
+//         return ans;
+        
+        
+        
+        vector<vector<int>>dp(grid.size(),vector<int>(grid[0].size(),INT_MAX));
+        dp[0]=grid[0];
+        for(int i=1;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                for(int k=0;k<grid[0].size();k++){
+                    dp[i][k]=min(dp[i][k],grid[i][k]+dp[i-1][j]+moveCost[grid[i-1][j]][k]);
+                }
+            }
+        }
+        return *min_element(dp[grid.size()-1].begin(),dp[grid.size()-1].end());
+        
+        
     }
 };
