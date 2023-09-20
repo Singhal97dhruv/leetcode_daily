6e5edb692a82e25544b2bbc5b5e0ca2f9a0ba533
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-09-21 00:26:25 +0530

    Time: 5 ms (77.91%), Space: 6.5 MB (64.08%) - LeetHub

--- 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
@@ -2,19 +2,25 @@ class Solution {
 public:
     
     int mod=1e9+7;
-    int dp[10001]={[0 ... 10000]=-1};
-    int recur(int idx,int n){
+    int dp[10001]={[0 ... 10000]=0};
+//     int recur(int idx,int n){
         
-        if(idx>=n)return 1;
-        if(dp[idx]!=-1)return dp[idx];
-        return dp[idx]= ((recur(idx+2,n))+recur(idx+1,n))%mod;
+//         if(idx>=n)return 1;
+//         if(dp[idx]!=-1)return dp[idx];
+//         return dp[idx]= ((recur(idx+2,n))+recur(idx+1,n))%mod;
         
         
-    }
+//     }
     
     
     int countHousePlacements(int n) {
-        int ans=recur(0,n);
-        return (1LL*ans*ans)%mod;
+        // int ans=recur(0,n);
+        // return (1LL*ans*ans)%mod;
+        dp[n]=1;
+        dp[n-1]=2;
+        for(int i=n-2;i>=0;i--){
+            dp[i]=(dp[i+1]+dp[i+2])%mod;
+        }
+        return (1LL*dp[0]*dp[0])%mod;
     }
 };
