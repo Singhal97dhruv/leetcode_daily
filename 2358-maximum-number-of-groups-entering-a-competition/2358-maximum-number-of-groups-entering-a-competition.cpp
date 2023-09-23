93195da65d50bc49c21afcfceb7c3effeffc39ef
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-09-23 12:27:04 +0530

    Time: 72 ms (60.33%), Space: 56.7 MB (10.39%) - LeetHub

--- 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.cpp
+++ 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.cpp
@@ -2,24 +2,30 @@ class Solution {
 public:
     int maximumGroups(vector<int>& grades) {
         
-        sort(grades.begin(),grades.end());
+//         sort(grades.begin(),grades.end());
        
-        int prevCnt=-1,prevScore=0;
-         int score=0,cnt=0,grp=0;
-        for(int i=0;i<grades.size();i++){
+//         int prevCnt=-1,prevScore=0;
+//          int score=0,cnt=0,grp=0;
+//         for(int i=0;i<grades.size();i++){
 
-            score+=grades[i];
-            cnt++;
-            if(cnt>prevCnt && score>prevScore){
-                grp++;
-                prevCnt=cnt;
-                prevScore =score;
-                cnt=score=0;
-            }
+//             score+=grades[i];
+//             cnt++;
+//             if(cnt>prevCnt && score>prevScore){
+//                 grp++;
+//                 prevCnt=cnt;
+//                 prevScore =score;
+//                 cnt=score=0;
+//             }
             
+//         }
+//         return grp;
+        
+        
+        int grps=0,temp=0;
+
+        while(temp<=grades.size()){
+            temp+=++grps;
         }
-        return grp;
-        
-        
+        return grps-1;
     }
 };
