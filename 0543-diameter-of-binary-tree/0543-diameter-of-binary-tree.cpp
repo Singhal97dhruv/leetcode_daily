ae2e81d7bdc096e84a2c9559aa1050dcc91e87e8
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-09-30 00:39:13 +0530

    Time: 6 ms (71.07%), Space: 20.5 MB (22.56%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -14,13 +14,20 @@ public:
     int ans=0;
     
     int dfs(TreeNode*t){
-        if(!t)return 0;
+//         if(!t)return 0;
         
+//         int left=dfs(t->left);
+//         int right=dfs(t->right);
+        
+//         ans=max(ans,abs(left+right));
+        
+//         return 1+max(left,right);
+        
+        if(!t)return 0;
         int left=dfs(t->left);
         int right=dfs(t->right);
         
-        ans=max(ans,abs(left+right));
-        
+        ans=max(ans,left+right);
         return 1+max(left,right);
         
     } 
