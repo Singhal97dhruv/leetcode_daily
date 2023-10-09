49d851d0fc56cb8b670c872052420223ecc21aa4
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-10-09 23:44:25 +0530

    Time: 11 ms (9.25%), Space: 14.2 MB (8.57%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     
-    int BinarySearch(vector<int>&nums,int target){
-        if(!nums.size())return -1;
-        int left=0,right=nums.size()-1;
-        while(left<right){
-            int mid=left+(right-left)/2;
-            if(nums[mid]==target)return mid;
-            if(target<nums[mid])right=mid;
-            else left=mid+1;
-        }
-        return nums[left]==target?left:-1;
-        
-    }
+   
     
     vector<int> searchRange(vector<int>& nums, int target) {
         
-        int idx=BinarySearch(nums,target);
+       int l=0,r=nums.size()-1,idx=-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]==target){
+                idx=mid;break;
+            }
+            if(target<nums[mid])r=mid-1;
+            else l=mid+1;
+        }
         if(idx==-1)return {-1,-1};
-        
-        int left=idx-1,right=idx+1;
-        while(left>=0 && nums[left]==nums[left+1])left--;
-
-        while(right<nums.size() && nums[right]==nums[right-1])right++;
-        
-        return {left+1,right-1};
-        
+        vector<int>ans;
+        int left=idx,right=idx;
+        while(left>=0 && nums[left]==target)left--;
+        ans.push_back(left+1);
+        while(right<nums.size() && nums[right]==target)right++;
+        ans.push_back(right-1);
+        return ans;
         
     }
 };
