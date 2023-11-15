f57b529f1f1e8e2366cad6e6e13c7c980303094a
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-11-15 08:53:08 +0530

    Time: 249 ms (76.74%), Space: 13.4 MB (66.51%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        int first[26] = {[0 ... 25] = INT_MAX}, last[26] = {}, res = 0;
-    for (int i = 0; i < s.size(); ++i) {
-        first[s[i] - 'a'] = min(first[s[i] - 'a'], i);
-        last[s[i] - 'a'] = i;
-    }
-    for (int i = 0; i < 26; ++i)
-        if (first[i] < last[i])
-            res += unordered_set<char>(begin(s) + first[i] + 1, begin(s) + last[i]).size();
-    return res;
+    //     int first[26] = {[0 ... 25] = INT_MAX}, last[26] = {}, res = 0;
+    // for (int i = 0; i < s.size(); ++i) {
+    //     first[s[i] - 'a'] = min(first[s[i] - 'a'], i);
+    //     last[s[i] - 'a'] = i;
+    // }
+    // for (int i = 0; i < 26; ++i)
+    //     if (first[i] < last[i])
+    //         res += unordered_set<char>(begin(s) + first[i] + 1, begin(s) + last[i]).size();
+    // return res;
+        
+        
+        int fst[26]={[0 ... 25]=INT_MAX},lst[26]={},res=0;
+        for(int i=0;i<s.size();i++){
+            fst[s[i]-'a']=min(fst[s[i]-'a'],i);
+            lst[s[i]-'a']=i;
+        }
+        
+        for(int i=0;i<26;i++){
+            if(fst[i]<lst[i])
+            res+=unordered_set<int>(s.begin()+fst[i]+1,s.begin()+lst[i]).size();
+        }
+        
+        return res;        
     }
 };
