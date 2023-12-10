feea7d671fb765b877fcf8a723f9f5791792e4ff
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-12-11 01:55:06 +0530

    Time: 12 ms (69.68%), Space: 12.7 MB (65.34%) - LeetHub

--- 1300-sum-of-mutated-array-closest-to-target/1300-sum-of-mutated-array-closest-to-target.cpp
+++ 1300-sum-of-mutated-array-closest-to-target/1300-sum-of-mutated-array-closest-to-target.cpp
@@ -3,11 +3,11 @@ public:
     int findBestValue(vector<int>& arr, int target) {
         
         sort(arr.begin(),arr.end());
-        int i,n=arr.size();
-        for(i=0;i<n;i++){
+        int i=0,n=arr.size();
+        for(i;i<n;i++){
             if(target<=arr[i]*(n-i))break;
             target-=arr[i];
         }
-        return i==n?arr[n-1]:int(round((target-0.01)/(n-i)));
+        return i==n?arr[n-1]:(round((target-0.01)/(n-i)));
     }
 };
