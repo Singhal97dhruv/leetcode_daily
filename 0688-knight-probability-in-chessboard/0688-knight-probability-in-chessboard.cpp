e1626297c25c0362c2a9827c42370c69ac1e06ca
Dhruv Singhal <91372434+Singhal97dhruv@users.noreply.github.com>
2023-12-13 01:47:42 +0530

    Time: 7 ms (81.53%), Space: 7.1 MB (75.16%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
@@ -1,41 +1,26 @@
 class Solution {
 public:
-    
-//     vector<vector<int>>offset={{2,-1},{2,1},{-2,1},{-2,-1},{1,2},{-1,2},{1,-2},{-1,-2}};
-    vector<vector<vector<double>>>dp;
-//     double recur(int n,int k,int row,int col){
-        
-//         if(row<0||col<0||row>=n||col>=n)return 0;
-//         if(k==0)return 1;
-//         if(dp[row][col][k]!=0)return dp[row][col][k];
-//         double temp=0;
-//         for(int i=0;i<8;i++){
-//             temp+=0.125*recur(n,k-1,row+offset[i][0],col+offset[i][1]);
-//         }
-//         return dp[row][col][k]=temp;
-//     }
-    
-    vector<vector<int>>offset={{2,1},{2,-1},{-2,-1},{-2,1},{1,2},{1,-2},{-1,2},{-1,-2}};
-    
-    double memo(int n,int k,int row,int col){
-        if(row<0 || col<0 || row>=n|| col>=n)return 0;
+    double dp[26][26][101];
+   vector<vector<int>>offset={{2,1},{2,-1},{-2,-1},{-2,1},{1,2},{1,-2},{-1,2},{-1,-2}};
+    double recur(int n,int k,int row,int col){
+        if(row<0 || col<0 || row>=n || col>=n )return 0;
         if(k==0)return 1;
         if(dp[row][col][k]!=0)return dp[row][col][k];
         
-        double temp=0;
+        double ans=0;
         for(int i=0;i<8;i++){
-            temp+=0.125*memo(n,k-1,row+offset[i][0],col+offset[i][1]);
+            ans+=0.125*recur(n,k-1,row+offset[i][0],col+offset[i][1]);
         }
-        return dp[row][col][k]= temp;
-        
+        return dp[row][col][k]= ans;
     }
     
     
     double knightProbability(int n, int k, int row, int column) {
-        dp.resize(n,vector<vector<double>>(n,vector<double>(k+1)));
+     
+        memset(dp,0.0,sizeof(dp));
         
-        // return recur(n,k,row,column);
-        return memo(n,k,row,column);
+        return recur(n,k,row,column);
+        
         
     }
 };
